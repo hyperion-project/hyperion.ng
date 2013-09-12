94a8dfad2f89bf164fc1b58c55b5ef65dc2ef35e
T. van der Zwan <zwantvd@gmail.com>
2013-09-12 18:42:16 +0000

    Enabled KnightRider boot sequence

--- libsrc/bootsequence/BootSequenceFactory.cpp
+++ libsrc/bootsequence/BootSequenceFactory.cpp
@@ -4,6 +4,7 @@
 
 // Local Bootsequence includes
 #include "RainbowBootSequence.h"
+#include "KittBootSequence.h"
 
 BootSequence * BootSequenceFactory::createBootSequence(Hyperion * hyperion, const Json::Value & jsonConfig)
 {
@@ -20,11 +21,8 @@ BootSequence * BootSequenceFactory::createBootSequence(Hyperion * hyperion, cons
 	}
 	else if (type == "knightrider")
 	{
-		std::cout << "KNIGHT RIDER NOT IMPLEMENTED YET" << std::endl;
-		return nullptr;
-//		const unsigned duration_ms = jsonConfig["duration_ms"].asUint();
-//		const std::string colorStr = jsonConfig["color"].asString();
-//		return new KnightRiderSequence(hyperion, duration_ms);
+		const unsigned duration_ms = jsonConfig["duration_ms"].asUInt();
+		return new KittBootSequence(hyperion, duration_ms);
 	}
 
 	std::cerr << "Unknown boot-sequence selected; boot-sequence disabled." << std::endl;
