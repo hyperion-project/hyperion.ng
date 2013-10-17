8d53c765c3e50e253386881131082578ec956abe
T. van der Zwan <zwantvd@gmail.com>
2013-10-17 07:43:10 +0000

    Added full description to create cross compile environment and build.
    Former-commit-id: 0000306b798ede4dbcc5643b09375c90c36c10b2

---
+++ Toolchain-RaspberryPi.cmake
@@ -0,0 +1,19 @@
+SET(RASPCROSS_DIR $ENV{HOME}/raspberrypi)
+
+SET(CMAKE_SYSTEM_NAME Linux) 
+SET(CMAKE_SYSTEM_VERSION 1) 
+
+# specify the cross compiler 
+SET(CMAKE_C_COMPILER   ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc) 
+SET(CMAKE_CXX_COMPILER ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++) 
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH ${RASPCROSS_DIR}/rootfs) 
+
+# search for programs in the build host directories 
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# for libraries and headers in the target directories 
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY) 
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
