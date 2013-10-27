73243eb593d9acaf3f00fc96f0435421d1931606
johan <johanpol@hotmail.com>
2013-10-27 13:15:48 +0100

    Fixed generation of device config type string
    Former-commit-id: ec245b9fae3f2bae690e985657807389770539d9

--- src/config-tool/ConfigTool/src/org/hyperion/hypercon/spec/DeviceConfig.java
+++ src/config-tool/ConfigTool/src/org/hyperion/hypercon/spec/DeviceConfig.java
@@ -37,7 +37,7 @@ public class DeviceConfig {
 		strBuf.append("\t{\n");
 		
 		strBuf.append("\t\t\"name\"       : \"").append(mName).append("\",\n");
-		strBuf.append("\t\t\"type\"       : \"").append(mType).append("\",\n");
+		strBuf.append("\t\t\"type\"       : \"").append(mType.name()).append("\",\n");
 		strBuf.append("\t\t\"output\"     : \"").append(mOutput).append("\",\n");
 		strBuf.append("\t\t\"rate\"       : ").append(mBaudrate).append(",\n");
 		strBuf.append("\t\t\"bgr-output\" : ").append(mBgrOutput).append("\n");
