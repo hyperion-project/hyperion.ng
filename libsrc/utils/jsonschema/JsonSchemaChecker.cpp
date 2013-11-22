958feabf5bc377387314ac2c25ed71a0eb147cbf
T. van der Zwan <zwantvd@gmail.com>
2013-11-22 10:47:05 +0000

    Added double as possible type
    Former-commit-id: 0caa8d0cb81db4d6592db296cb1f775e91efce50

--- libsrc/utils/jsonschema/JsonSchemaChecker.cpp
+++ libsrc/utils/jsonschema/JsonSchemaChecker.cpp
@@ -154,6 +154,8 @@ void JsonSchemaChecker::checkType(const Json::Value & value, const Json::Value &
 		wrongType = !value.isNumeric();
 	else if (type == "integer")
 		wrongType = !value.isIntegral();
+	else if (type == "double")
+		wrongType = !value.isDouble();
 	else if (type == "boolean")
 		wrongType = !value.isBool();
 	else if (type == "object")
