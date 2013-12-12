155c2a0ae9d00647bacd66b2ea8182030a1dd9b0
T. van der Zwan <zwantvd@gmail.com>
2013-12-12 22:04:41 +0000

    Added 'lightberry' device
    Former-commit-id: 9ada3a0bb32e694b6e2f4da265421147559a117b

--- libsrc/hyperion/Hyperion.cpp
+++ libsrc/hyperion/Hyperion.cpp
@@ -40,7 +40,7 @@ LedDevice* Hyperion::createDevice(const Json::Value& deviceConfig)
 	std::transform(type.begin(), type.end(), type.begin(), ::tolower);
 
 	LedDevice* device = nullptr;
-	if (type == "ws2801")
+	if (type == "ws2801" || type == "lightberry")
 	{
 		const std::string output = deviceConfig["output"].asString();
 		const unsigned rate      = deviceConfig["rate"].asInt();
