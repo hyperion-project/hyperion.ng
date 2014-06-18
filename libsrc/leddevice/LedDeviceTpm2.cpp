3135b89255f529737da02ce4bde073328a5ee509
Gamadril <Gamadril@users.noreply.github.com>
2014-06-18 13:49:27 +0200

    Update LedDeviceTpm2.cpp
    Former-commit-id: 8d51eb00044460002e29687468786e3056afd0bb

--- libsrc/leddevice/LedDeviceTpm2.cpp
+++ libsrc/leddevice/LedDeviceTpm2.cpp
@@ -22,11 +22,11 @@ int LedDeviceTpm2::write(const std::vector<ColorRgb> &ledValues)
 {
   	if (_ledBuffer.size() == 0)
   	{
-    	_ledBuffer.resize(5 + 3*ledValues.size());
+    		_ledBuffer.resize(5 + 3*ledValues.size());
     		_ledBuffer[0] = 0xC9; // block-start byte
     		_ledBuffer[1] = 0xDA; // DATA frame
-    		_ledBuffer[2] = ((3 * ledValues.size()) >> 8) & 0xFF; // LED count high byte
-    		_ledBuffer[3] = (3 * ledValues.size()) & 0xFF; // LED count low byte
+    		_ledBuffer[2] = ((3 * ledValues.size()) >> 8) & 0xFF; // frame size high byte
+    		_ledBuffer[3] = (3 * ledValues.size()) & 0xFF; // frame size low byte
     		_ledBuffer.back() = 0x36; // block-end byte
   	}
 
