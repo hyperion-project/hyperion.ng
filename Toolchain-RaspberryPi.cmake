ae4462f098b56483bbc5c861a02bc05bd904a402
poljvd <johanpol@hotmail.com>
2014-12-20 20:51:17 +0100

    Update compile instructions
    Former-commit-id: b5e0ad129c174de32fbb814595aaefe1fc0f6c13

--- Toolchain-RaspberryPi.cmake
+++ Toolchain-RaspberryPi.cmake
@@ -4,8 +4,8 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1) 
 
 # specify the cross compiler 
-SET(CMAKE_C_COMPILER   ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc) 
-SET(CMAKE_CXX_COMPILER ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++) 
+SET(CMAKE_C_COMPILER   ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++)
 
 # where is the target environment 
 SET(CMAKE_FIND_ROOT_PATH ${RASPCROSS_DIR}/rootfs) 
