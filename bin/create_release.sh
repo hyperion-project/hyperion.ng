33d7e54f1f18846819acc85c6dc4e6e5dd7be21a
brindosch <edeltraud70@gmx.de>
2016-03-13 17:56:00 +0100

    fix createrelease.sh
    Former-commit-id: d004178819bb9ac41b042149da3b5ef30a43d890

--- bin/create_release.sh
+++ bin/create_release.sh
@@ -14,60 +14,7 @@ if ! [ -d "$builddir" ]; then
 	echo "Could not find build director"
 	exit 1
 fi
-
-#Create RPI .tar.gz
-if [ "$buildid" = "rpi" ]; then
-outfile="$repodir/deploy/hyperion_$buildid.tar.gz"
-echo create $outfile
-
-tar --create --verbose --gzip --absolute-names --show-transformed-names --ignore-failed-read\
-	--file "$outfile" \
-	--transform "s:$builddir/bin/:hyperion/bin/:" \
-	--transform "s:$repodir/effects/:hyperion/effects/:" \
-	--transform "s:$repodir/config/:hyperion/config/:" \
-	--transform "s:$repodir/bin/service/hyperion.init.sh:hyperion/init.d/hyperion.init.sh:" \
-	--transform "s:$repodir/bin/service/hyperion.systemd.sh:hyperion/init.d/hyperion.systemd.sh:" \
-	--transform "s:$repodir/bin/service/hyperion.initctl.sh:hyperion/init.d/hyperion.initctl.sh:" \
-	--transform "s://:/:g" \
-	"$builddir/bin/hyperiond" \
-	"$builddir/bin/hyperion-remote" \
-	"$builddir/bin/hyperion-v4l2" \
-	"$builddir/bin/hyperion-dispmanx" \
-	"$repodir/effects/"* \
-	"$repodir/bin/service/hyperion.init.sh" \
-	"$repodir/bin/service/hyperion.systemd.sh" \
-	"$repodir/bin/service/hyperion.initctl.sh" \
-	"$repodir/config/hyperion.config.json"
-fi
-exit 0
-
-#Create Wetek imx6 .tar.gz
-if [ "$buildid" = "wetek" ] || [ "$buildid" = "imx6" ]; then
-outfile="$repodir/deploy/hyperion_$buildid.tar.gz"
-echo create $outfile
-
-tar --create --verbose --gzip --absolute-names --show-transformed-names --ignore-failed-read\
-	--file "$outfile" \
-	--transform "s:$builddir/bin/:hyperion/bin/:" \
-	--transform "s:$repodir/effects/:hyperion/effects/:" \
-	--transform "s:$repodir/config/:hyperion/config/:" \
-	--transform "s:$repodir/bin/service/hyperion.init.sh:hyperion/init.d/hyperion.init.sh:" \
-	--transform "s:$repodir/bin/service/hyperion.systemd.sh:hyperion/init.d/hyperion.systemd.sh:" \
-	--transform "s:$repodir/bin/service/hyperion.initctl.sh:hyperion/init.d/hyperion.initctl.sh:" \
-	--transform "s://:/:g" \
-	"$builddir/bin/hyperiond" \
-	"$builddir/bin/hyperion-remote" \
-	"$builddir/bin/hyperion-v4l2" \
-	"$repodir/effects/"* \
-	"$repodir/bin/service/hyperion.init.sh" \
-	"$repodir/bin/service/hyperion.systemd.sh" \
-	"$repodir/bin/service/hyperion.initctl.sh" \
-	"$repodir/config/hyperion.config.json"
-fi
-exit 0
-
-#Create x32 and x64 .tar.gz
-if [ "$buildid" = "x32" ] || [ "$buildid" = "x64" ]; then
+	
 outfile="$repodir/deploy/hyperion_$buildid.tar.gz"
 echo create $outfile
 
@@ -84,10 +31,10 @@ tar --create --verbose --gzip --absolute-names --show-transformed-names --ignore
 	"$builddir/bin/hyperion-remote" \
 	"$builddir/bin/hyperion-v4l2" \
 	"$builddir/bin/hyperion-x11" \
+	"$builddir/bin/hyperion-dispmanx" \
 	"$repodir/effects/"* \
 	"$repodir/bin/service/hyperion.init.sh" \
 	"$repodir/bin/service/hyperion.systemd.sh" \
 	"$repodir/bin/service/hyperion.initctl.sh" \
 	"$repodir/config/hyperion.config.json"
-fi
-exit 0
+
