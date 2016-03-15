f783c59185c3274b47923b8efaf0ee0e863f0357
RickDB <RickDB@gmail.com>
2016-03-15 12:06:24 +0100

    Corrected indenting in LedDeviceFactory.cpp.
    Former-commit-id: 31713a4a0ce9bd25fd1e39969bbb481363a80167

--- libsrc/leddevice/LedDeviceFactory.cpp
+++ libsrc/leddevice/LedDeviceFactory.cpp
@@ -248,15 +248,15 @@ LedDevice * LedDeviceFactory::construct(const Json::Value & deviceConfig)
 	}
   else if (type == "atmoorb")
   {
-    const std::string output = deviceConfig["output"].asString();
-    const bool switchOffOnBlack = deviceConfig.get("switchOffOnBlack", true).asBool();
-    const int transitiontime = deviceConfig.get("transitiontime", 1).asInt();
-    const int port = deviceConfig.get("port", 1).asInt();
-    const int numLeds = deviceConfig.get("numLeds", 1).asInt();
+		const std::string output = deviceConfig["output"].asString();
+		const bool switchOffOnBlack = deviceConfig.get("switchOffOnBlack", true).asBool();
+		const int transitiontime = deviceConfig.get("transitiontime", 1).asInt();
+		const int port = deviceConfig.get("port", 1).asInt();
+		const int numLeds = deviceConfig.get("numLeds", 1).asInt();
 
-    std::vector<unsigned int> orbIds;
-    for (Json::Value::ArrayIndex i = 0; i < deviceConfig["orbIds"].size(); i++) {
-      orbIds.push_back(deviceConfig["orbIds"][i].asInt());
+		std::vector<unsigned int> orbIds;
+		for (Json::Value::ArrayIndex i = 0; i < deviceConfig["orbIds"].size(); i++) {
+		  orbIds.push_back(deviceConfig["orbIds"][i].asInt());
     }
 
     device = new LedDeviceAtmoOrb(output, switchOffOnBlack, transitiontime, port, numLeds, orbIds);
