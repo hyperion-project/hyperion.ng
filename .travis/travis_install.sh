2c1b8cf41930a6247f5d141c1d3d299d46f98c35
brindosch <edeltraud70@gmx.de>
2016-06-28 18:21:59 +0200

    maybe fix

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # install osx deps for hyperion compile
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	echo "Install OSX deps"
 	brew update
 	brew install qt5
 	brew install libusb
@@ -8,6 +9,7 @@ fi
 
 # install linux deps for hyperion compile
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+	echo "Install linux deps"
 	sudo apt-get -qq update ; sudo apt-get install -qq -y qtbase5-dev libqt5serialport5-dev libusb-1.0-0-dev python-dev libxrender-dev libavahi-core-dev libavahi-compat-libdnssd-dev
 fi
 
