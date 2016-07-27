83fb5ed8c87333fba5976e2687e8784aa89aaf9d
redpanther <redpanther@spooky-online.de>
2016-07-27 08:14:38 +0200

    tune code formating

--- libsrc/grabber/amlogic/AmlogicWrapper.cpp
+++ libsrc/grabber/amlogic/AmlogicWrapper.cpp
@@ -11,16 +11,16 @@
 #include <grabber/AmlogicGrabber.h>
 
 
-AmlogicWrapper::AmlogicWrapper(const unsigned grabWidth, const unsigned grabHeight, const unsigned updateRate_Hz, const int priority) :
-	_updateInterval_ms(1000/updateRate_Hz),
-	_timeout_ms(2 * _updateInterval_ms),
-	_priority(priority),
-	_timer(),
-	_image(grabWidth, grabHeight),
-	_frameGrabber(new AmlogicGrabber(grabWidth, grabHeight)),
-	_processor(ImageProcessorFactory::getInstance().newImageProcessor()),
-	_ledColors(Hyperion::getInstance()->getLedCount(), ColorRgb{0,0,0}),
-	_hyperion(Hyperion::getInstance())
+AmlogicWrapper::AmlogicWrapper(const unsigned grabWidth, const unsigned grabHeight, const unsigned updateRate_Hz, const int priority)
+	: _updateInterval_ms(1000/updateRate_Hz)
+	, _timeout_ms(2 * _updateInterval_ms)
+	, _priority(priority)
+	, _timer()
+	, _image(grabWidth, grabHeight)
+	, _frameGrabber(new AmlogicGrabber(grabWidth, grabHeight))
+	, _processor(ImageProcessorFactory::getInstance().newImageProcessor())
+	, _ledColors(Hyperion::getInstance()->getLedCount(), ColorRgb{0,0,0})
+	, _hyperion(Hyperion::getInstance())
 {
 	// Configure the timer to generate events every n milliseconds
 	_timer.setInterval(_updateInterval_ms);
