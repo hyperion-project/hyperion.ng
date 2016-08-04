4baee313b1f39c806350a6552db4a2b67503b6db
Paulchen-Panther <Paulchen--Panter@gmx.net>
2016-08-04 11:02:55 +0200

    Add files via upload

---
+++ include/utils/Components.h
@@ -0,0 +1,30 @@
+#pragma once
+
+/**
+ * Enumeration of components in Hyperion.
+ */
+enum Components
+{
+	SMOOTHING,
+	BLACKBORDER,
+	KODICHECKER,
+	FORWARDER,
+	UDPLISTENER,
+	BOBLIGHTSERVER,
+	GRABBER
+};
+
+inline const char* componentToString(Components c)
+{
+	switch (c)
+	{
+		case SMOOTHING:		return "Smoothing option";
+		case BLACKBORDER:	return "Blackborder detector";
+		case KODICHECKER:	return "KodiVideoChecker";
+		case FORWARDER:		return "Json/Proto forwarder";
+		case UDPLISTENER:	return "UDP listener";
+		case BOBLIGHTSERVER:	return "Boblight server";
+		case GRABBER:		return "Framegrabber";
+		default:		return "";
+	}
+}
