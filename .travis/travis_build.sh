e7cc20eb99288841dfdfec676ec428ed874ebe69
Rick van Hattem <Wolph@wol.ph>
2016-08-06 04:32:07 +0200

    adding OS X travis test

--- .travis/travis_build.sh
+++ .travis/travis_build.sh
@@ -10,11 +10,11 @@
 # compile hyperion on osx
 if [[ $TRAVIS_OS_NAME == 'osx' ]]
 then
-	cmake . -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.6.1-1
+	cmake . -DCMAKE_PREFIX_PATH=$(ls /usr/local/Cellar/qt5/ | sort -nr | head -n 1)
 	mkdir build || exit 1
 	cd build
 	cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTS=ON -Wno-dev .. || exit 2
-	make -j$(nproc) || exit 3
+	make -j$(sysctl -n hw.ncpu) || exit 3
 	# make -j$(nproc) package || exit 4 # currently osx(dmg) package creation not implemented
 fi
 
--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -3,14 +3,13 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	echo "Install OSX deps"
 	brew update
-	brew install qt5
-	brew install libusb
-	brew install cmake
+	brew install libusb cmake qt5
 fi
 
 # install linux deps for hyperion compile
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	echo "Install linux deps"
-	sudo apt-get -qq update ; sudo apt-get install -qq -y qtbase5-dev libqt5serialport5-dev libusb-1.0-0-dev python-dev libxrender-dev libavahi-core-dev libavahi-compat-libdnssd-dev doxygen
+	sudo apt-get -qq update
+	sudo apt-get install -qq -y qtbase5-dev libqt5serialport5-dev libusb-1.0-0-dev python-dev libxrender-dev libavahi-core-dev libavahi-compat-libdnssd-dev doxygen
 fi
 
