d1e7132f5ba8af073d4dbb1e43cfca6305a56903
brindosch <edeltraud70@gmx.de>
2016-08-31 22:58:23 +0200

    .

--- .travis/travis_deploy.sh
+++ .travis/travis_deploy.sh
@@ -21,5 +21,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 		sf_upload $TRAVIS_BUILD_DIR/afile nightly
 	elif [[ $TRAVIS_EVENT_TYPE == 'cron' ]]; then
 		sf_upload abinary nightly
-	fi
+	else
+		echo "PR upload"
+		echo "build dir: ${TRAVIS_BUILD_DIR}"
+		sf_upload /home/travis/build/brindosch/hyperion.ngBeta/build/hyperion-2.0.0-Linux-x86-dev.deb beta
 fi
