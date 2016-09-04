b99e75d7e4baa938c17330783773678d583735e0
b1rdhous3 <jan-philipp.fischer@greencoding.de>
2016-09-04 16:27:48 +0200

    Fixing Safari Support (#222)
    * Switch to non-minified JSON Editor Lib
    
    * Fixing Safari Support

--- assets/webconfig/js/ui_utils.js
+++ assets/webconfig/js/ui_utils.js
@@ -1,10 +1,10 @@
 
-function bindNavToContent(containerId, fileName, loadNow=false)
+function bindNavToContent(containerId, fileName, loadNow)
 {
 	$("#page-wrapper").off();
 	$(containerId).on("click", function() {
 		$("#page-wrapper").load("/content/"+fileName+".html");
-	}); 
+	});
 	if (loadNow)
 	{
 		$("#page-wrapper").load("/content/"+fileName+".html");
