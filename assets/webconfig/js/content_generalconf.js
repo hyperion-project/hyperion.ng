88f5c4d6f35bc9b9ba223dde1dbe8df9cf6323d0
b1rdhous3 <jan-philipp.fischer@t-online.de>
2016-09-08 00:41:57 +0200

    Initial JSON Write

--- assets/webconfig/js/content_generalconf.js
+++ assets/webconfig/js/content_generalconf.js
@@ -25,23 +25,23 @@ $(hyperion).one("cmd-config-getschema", function(event) {
 	//removeAdvanced(parsedConfSchemaJSON, []); // not working atm
 	//console.log(JSON.stringify(parsedConfSchemaJSON));
 	schema = parsedConfSchemaJSON.properties;
-	schema_blackborderdetector = schema.blackborderdetector;
-	schema_color = schema.color;
-	schema_effects = schema.effects;
-	schema_forwarder = schema.forwarder;
-	schema_initialEffect = schema.initialEffect;
-	schema_kodiVideoChecker = schema.kodiVideoChecker;
-	schema_smoothing = schema.smoothing;
-	schema_logger = schema.logger;
-	schema_jsonServer = schema.jsonServer;
-	schema_protoServer = schema.protoServer;
-	schema_boblightServer = schema.boblightServer;
-	schema_udpListener = schema.udpListener;
-	schema_webConfig = schema.webConfig;
+	blackborderdetector = schema.blackborderdetector;
+	color = schema.color;
+	effects = schema.effects;
+	forwarder = schema.forwarder;
+	initialEffect = schema.initialEffect;
+	kodiVideoChecker = schema.kodiVideoChecker;
+	smoothing = schema.smoothing;
+	logger = schema.logger;
+	jsonServer = schema.jsonServer;
+	protoServer = schema.protoServer;
+	boblightServer = schema.boblightServer;
+	udpListener = schema.udpListener;
+	webConfig = schema.webConfig;
 
 	var element = document.getElementById('editor_holder');
 	//JSONEditor.defaults.options.theme = 'bootstrap3';
-	
+
 	var general_conf_editor = new JSONEditor(element,{
 		theme: 'bootstrap3',
 		disable_collapse: 'true',
@@ -52,22 +52,32 @@ $(hyperion).one("cmd-config-getschema", function(event) {
 		schema: {
 			title:' ',
 			properties: {
-				schema_blackborderdetector,
-				schema_color,
-				schema_effects,
-				schema_forwarder,
-				schema_initialEffect,
-				schema_kodiVideoChecker,
-				schema_smoothing,
-				schema_logger,
-				schema_jsonServer,
-				schema_protoServer,
-				schema_boblightServer,
-				schema_udpListener,
-				schema_webConfig
+				blackborderdetector//,
+				/*color,
+				effects,
+				forwarder,
+				initialEffect,
+				kodiVideoChecker,
+				smoothing,
+				logger,
+				jsonServer,
+				protoServer,
+				boblightServer,
+				udpListener,
+				webConfig*/
 			}
 		}
 	});
+
+	general_conf_editor.on('change',function() {
+		/*var name = general_conf_editor.getEditor('root.schema_blackborderdetector');
+	  console.log(name.getValue());
+*/
+	console.log(JSON.stringify(general_conf_editor.getValue()));
+		//console.log('schema_blackborderdetector'.substr(7));
+		websocket.send('{"command":"config","subcommand":"setconfig","config":"'+JSON.stringify(general_conf_editor.getValue())+'","create":false, "overwrite":false}');
+	});
+
 });
 
 
@@ -80,4 +90,3 @@ $(document).ready( function() {
 	});
 //  $("[type='checkbox']").bootstrapSwitch();
 });
-
