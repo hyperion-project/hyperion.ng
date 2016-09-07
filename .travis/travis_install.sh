14b15cc436edf278a7550ced62906b36a847a49a
brindosch <edeltraud70@gmx.de>
2016-09-07 23:11:08 +0200

    Travisci: sf deployment (#215)

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -18,7 +18,7 @@ elif [[ $TRAVIS_OS_NAME == 'linux' ]]
 then
 	echo "Install linux deps"
 	sudo apt-get -qq update
-	sudo apt-get install -qq -y qtbase5-dev libqt5serialport5-dev libusb-1.0-0-dev python-dev libxrender-dev libavahi-core-dev libavahi-compat-libdnssd-dev doxygen
+	sudo apt-get install -qq -y qtbase5-dev libqt5serialport5-dev libusb-1.0-0-dev python-dev libxrender-dev libavahi-core-dev libavahi-compat-libdnssd-dev doxygen expect
 else
     echo "Unsupported platform: $TRAVIS_OS_NAME"
     exit 5
