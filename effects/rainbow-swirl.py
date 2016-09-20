b108bb49a6952594dece95219d096b7847ad1417
redpanther <redpanther@spooky-online.de>
2016-09-20 20:13:31 +0200

    fix rainbow effect

--- effects/rainbow-swirl.py
+++ effects/rainbow-swirl.py
@@ -8,15 +8,9 @@ centerY      = float(hyperion.args.get('center_y', 0.5))
 
 # Check parameters
 rotationTime = max(0.1, rotationTime)
-angle = 0
-
-# Calculate the sleep time and rotation increment
-increment = 3
 sleepTime = rotationTime / 360
-while sleepTime < 0.05:
-	increment *= 2
-	sleepTime *= 2
-increment %= 360
+angle = 0
+increment = 1
 
 # table of stop colors for rainbow gradient
 rainbowColors = bytearray([
@@ -32,7 +26,7 @@ rainbowColors = bytearray([
 ])
 
 if reverse:
-	increment = -increment
+	increment *= -1
 
 while not hyperion.abort():
 	angle += increment
@@ -43,7 +37,7 @@ while not hyperion.abort():
 		hyperion.imageWidth,hyperion.imageHeight,
 		int(round(hyperion.imageWidth)*centerX),int(round(float(hyperion.imageHeight)*centerY)),
 		angle,rainbowColors
-	);
+	)
 
 	hyperion.imageShow()
 	time.sleep(sleepTime)
