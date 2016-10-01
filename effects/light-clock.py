5793236ed536547cab81db3718958a59d47adbc6
redpanther <redpanther@spooky-online.de>
2016-10-01 18:37:33 +0200

    make seconds hand optional and change color to yellow make blue hours hand always visible

--- effects/light-clock.py
+++ effects/light-clock.py
@@ -1,14 +1,13 @@
 import hyperion, time, datetime
 
 # Get the parameters
-centerX   = int(round(hyperion.imageWidth)/2)
-centerY   = int(round(float(hyperion.imageHeight)/2))
-
-# table of stop colors for rainbow gradient, first is the position, next rgb, all values 0-255
+showSeconds = bool(hyperion.args.get('show_seconds', True))
+centerX     = int(round(hyperion.imageWidth)/2)
+centerY     = int(round(float(hyperion.imageHeight)/2))
 
 colorsSecond = bytearray([
-	0, 255,0,0,255,
-	5, 255,0,0,255,
+	0, 255,255,0,255,
+	5, 255,255,0,255,
 	30, 0,0,0,0,
 ])
 
@@ -22,27 +21,34 @@ colorsMinute = bytearray([
 colorsHour = bytearray([
 	0, 0,0,255,255,
 	10, 0,0,255,255,
-	255, 0,0,255,5,
+	127, 0,0,196,127,
+	255, 0,0,196,5,
 ])
 
-
+colorsHourTop = bytearray([
+	0, 0,0,255,250,
+	10, 0,0,255,128,
+	20, 0,0,0,0,
+])
 
 # effect loop
 while not hyperion.abort():
 	now = datetime.datetime.now()
 
-	angleH = 359 - 30*(now.hour if now.hour<12 else now.hour-12)+90
-	angleM = 359 - 6*now.minute+90
-	angleS = 359 - 6*now.second+90
+	angleH = 449 - 30*(now.hour if now.hour<12 else now.hour-12)
+	angleM = 449 - 6*now.minute
+	angleS = 449 - 6*now.second
 
-	angleH = angleH if angleH<360 else angleH-360
-	angleM = angleM if angleM<360 else angleM-360
-	angleS = angleS if angleS<360 else angleS-360
+	angleH -= 0 if angleH<360 else 360
+	angleM -= 0 if angleM<360 else 360
+	angleS -= 0 if angleS<360 else 360
 
-	hyperion.imageSolidFill(145,145,64);
+	hyperion.imageSolidFill(127,127,127);
 	hyperion.imageCanonicalGradient(centerX, centerY, angleH, colorsHour)
 	hyperion.imageCanonicalGradient(centerX, centerY, angleM, colorsMinute)
-	hyperion.imageCanonicalGradient(centerX, centerY, angleS, colorsSecond)
+	hyperion.imageCanonicalGradient(centerX, centerY, angleH, colorsHourTop)
+	if showSeconds:
+		hyperion.imageCanonicalGradient(centerX, centerY, angleS, colorsSecond)
 
 	hyperion.imageShow()
 	time.sleep(0.01 )
