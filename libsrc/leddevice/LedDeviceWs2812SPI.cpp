c8449e98e0a8bf371cfbdfbd100089b2bf05a36f
penfold42 <penfold42@users.noreply.github.com>
2016-12-02 00:25:11 +1100

    sk6812SPI was buggy cleaned up code - make sk6812spi and ws2812spi more consistent

--- libsrc/leddevice/LedDeviceWs2812SPI.cpp
+++ libsrc/leddevice/LedDeviceWs2812SPI.cpp
@@ -2,6 +2,7 @@
 
 LedDeviceWs2812SPI::LedDeviceWs2812SPI(const QJsonObject &deviceConfig)
 	: ProviderSpi()
+	, SPI_BYTES_PER_COLOUR(4)
 	, bitpair_to_byte {
 		0b10001000,
 		0b10001100,
@@ -20,29 +21,28 @@ LedDevice* LedDeviceWs2812SPI::construct(const QJsonObject &deviceConfig)
 bool LedDeviceWs2812SPI::init(const QJsonObject &deviceConfig)
 {
 	_baudRate_Hz = 3000000;
-	ProviderSpi::init(deviceConfig);
+	if ( !ProviderSpi::init(deviceConfig) )
+	{
+		return false;
+	}
 	WarningIf(( _baudRate_Hz < 2050000 || _baudRate_Hz > 4000000 ), _log, "SPI rate %d outside recommended range (2050000 -> 4000000)", _baudRate_Hz);
+
+	const int SPI_FRAME_END_LATCH_BYTES = 3;
+	_ledBuffer.resize(_ledRGBCount * SPI_BYTES_PER_COLOUR + SPI_FRAME_END_LATCH_BYTES, 0x00);
 
 	return true;
 }
 
 int LedDeviceWs2812SPI::write(const std::vector<ColorRgb> &ledValues)
 {
-	// 3 colours, 4 spi bytes per colour + 3 frame end latch bytes
-	const int SPI_BYTES_PER_LED  = 3 * 4;
-	unsigned spi_size = _ledCount * SPI_BYTES_PER_LED + 3;
-
-	if(_ledBuffer.size() != spi_size)
-	{
-		_ledBuffer.resize(spi_size, 0x00);
-	}
-
 	unsigned spi_ptr = 0;
-	for (unsigned i=0; i<(unsigned)_ledCount; ++i)
+	const int SPI_BYTES_PER_LED = sizeof(ColorRgb) * SPI_BYTES_PER_COLOUR;
+
+	for (const ColorRgb& color : ledValues)
 	{
-		uint32_t colorBits = ((unsigned int)ledValues[i].red << 16) 
-			| ((unsigned int)ledValues[i].green << 8) 
-			| ledValues[i].blue;
+		uint32_t colorBits = ((unsigned int)color.red << 16)
+			| ((unsigned int)color.green << 8)
+			| color.blue;
 
 		for (int j=SPI_BYTES_PER_LED - 1; j>=0; j--)
 		{
@@ -51,9 +51,10 @@ int LedDeviceWs2812SPI::write(const std::vector<ColorRgb> &ledValues)
 		}
 		spi_ptr += SPI_BYTES_PER_LED;
 	}
+
 	_ledBuffer[spi_ptr++] = 0;
 	_ledBuffer[spi_ptr++] = 0;
 	_ledBuffer[spi_ptr++] = 0;
 
-	return writeBytes(spi_size, _ledBuffer.data());
+	return writeBytes(_ledBuffer.size(), _ledBuffer.data());
 }
