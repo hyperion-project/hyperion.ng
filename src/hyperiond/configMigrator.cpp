3746f643afa82066dc4353ab5c98d150c93df4e0
redPanther <redPanther@users.noreply.github.com>
2016-12-24 10:02:12 +0100

    multi bugfix pr ... (#348)
    * move name to general
    add a version for config file
    
    * start impl. config migrator
    
    * fix typo amd set access level
    
    * fix schemaa name
    
    * fix schema
    
    * add structure for config migrator
    
    * add more structural things for config migrator
    
    * fix not all grabber provide live video
    
    * add schema for trails

--- src/hyperiond/configMigrator.cpp
+++ src/hyperiond/configMigrator.cpp
@@ -2,7 +2,6 @@
 
 
 ConfigMigrator::ConfigMigrator()
-	: _log(Logger::getInstance("ConfigMigrator"))
 {
 }
 
@@ -13,7 +12,23 @@ ConfigMigrator::~ConfigMigrator()
 bool ConfigMigrator::migrate(QString configFile, int fromVersion,int toVersion)
 {
 	Debug(_log, "migrate config %s from version %d to %d.", configFile.toLocal8Bit().constData(), fromVersion, toVersion);
-	throw std::runtime_error("ERROR: config migration not implemented");
+	
+	for (int v=fromVersion; v<toVersion; v++)
+	{
+		switch(v)
+		{
+			case 1: migrateFrom1(); break;
+
+			default:
+				throw std::runtime_error("ERROR: config migration - unknown version");
+		}
+	}
+	
 	return true;
 }
- 
+
+void ConfigMigrator::migrateFrom1()
+{
+	throw std::runtime_error("ERROR: config migration not implemented");
+}
+
