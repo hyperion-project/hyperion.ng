38ded183a915773b22bb8edd0be9e93baec9041f
redpanther <redpanther@spooky-online.de>
2016-12-29 10:28:05 +0100

    add effects checking

--- test/jsonchecks/checkeffects.py
+++ test/jsonchecks/checkeffects.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import json, sys, glob
 from os import path
+from jsonschema import Draft3Validator
+
 print("-- validate json files:")
 
 jsonFiles = sys.argv[1]
@@ -14,15 +16,27 @@ for filename in glob.glob(jsonFiles+'/*.json'):
 		total += 1
 		msg = "   check effect %s ... " % filename
 		try:
-			j      = json.load(f)
-			script = path.basename(j['script'])
+			effect = json.load(f)
+			script = path.basename(effect['script'])
 			if not path.exists(jsonFiles+'/'+script):
 				raise ValueError('script file: '+script+' not found.')
+
 			schema = path.splitext(script)[0]+'.schema.json'
 			if not path.exists(jsonFiles+'/schema/'+schema):
 				raise ValueError('schema file: '+schema+' not found.')
+			schema = jsonFiles+'/schema/'+schema
+			
+			# validate against schema
+			with open(schema) as s:
+				effectSchema = json.load(s)
+				Draft3Validator.check_schema(effectSchema)
+				validator = Draft3Validator(effectSchema)
+				validator.validate(effect)
+			
 			print(msg + "ok")
-		except ValueError as e:
+			
+			
+		except Exception as e:
 			print(msg + 'error ('+str(e)+')')
 			errors += 1
 			retval = 1
