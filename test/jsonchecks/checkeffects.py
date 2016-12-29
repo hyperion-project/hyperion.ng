de359da4f6199442f50b2399f8d4d74a64ea1d36
redpanther <redpanther@spooky-online.de>
2016-12-29 16:04:36 +0100

    better effects checking.

--- test/jsonchecks/checkeffects.py
+++ test/jsonchecks/checkeffects.py
@@ -11,37 +11,40 @@ schemaFiles = sys.argv[2]
 retval  = 0 
 total   = 0
 errors  = 0
-for filename in glob.glob(jsonFiles+'/*.json'):
-	with open(filename) as f:
-		total += 1
-		msg = "   check effect %s ... " % filename
-		try:
-			effect = json.load(f)
-			script = path.basename(effect['script'])
-			if not path.exists(jsonFiles+'/'+script):
-				raise ValueError('script file: '+script+' not found.')
+with open("libsrc/effectengine/EffectDefinition.schema.json") as baseSchemaFile:
+	baseSchema = json.load(baseSchemaFile)
+	baseValidator = Draft3Validator(baseSchema)
+	for filename in glob.glob(jsonFiles+'/*.json'):
+		with open(filename) as f:
+			total += 1
+			msg = "   check effect %s ... " % filename
+			try:
+				effect = json.load(f)
+				script = path.basename(effect['script'])
+				if not path.exists(jsonFiles+'/'+script):
+					raise ValueError('script file: '+script+' not found.')
 
-			schema = path.splitext(script)[0]+'.schema.json'
-			if not path.exists(jsonFiles+'/schema/'+schema):
-				raise ValueError('schema file: '+schema+' not found.')
-			schema = jsonFiles+'/schema/'+schema
-			
-			# validate against schema
-			with open(schema) as s:
-				effectSchema = json.load(s)
-				Draft3Validator.check_schema(effectSchema)
-				validator = Draft3Validator(effectSchema)
-				validator.validate(effect)
-			
-			print(msg + "ok")
-			
-			
-		except Exception as e:
-			print(msg + 'error ('+str(e)+')')
-			errors += 1
-			retval = 1
+				schema = path.splitext(script)[0]+'.schema.json'
+				if not path.exists(jsonFiles+'/schema/'+schema):
+					raise ValueError('schema file: '+schema+' not found.')
+				schema = jsonFiles+'/schema/'+schema
+				
+				# validate against schema
+				with open(schema) as s:
+					effectSchema = json.load(s)
+					Draft3Validator.check_schema(effectSchema)
+					validator = Draft3Validator(effectSchema)
+					baseValidator.validate(effect)
+					validator.validate(effect['args'])
+				
+				#print(msg + "ok")
+
+			except Exception as e:
+				print(msg + 'error ('+str(e)+')')
+				errors += 1
+				retval = 1
 			
 
-#print("   checked files: %s success: %s errors: %s" % (total,(total-errors),errors))
+print("   checked effect files: %s success: %s errors: %s" % (total,(total-errors),errors))
 
 sys.exit(retval)
