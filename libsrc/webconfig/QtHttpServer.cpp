e3be03ea73235df7fbab783ccafb8a42a85159bc
Paulchen Panther <Paulchen--Panter@gmx.net>
2018-12-20 15:16:23 +0100

    Close client connection on stop

--- libsrc/webconfig/QtHttpServer.cpp
+++ libsrc/webconfig/QtHttpServer.cpp
@@ -64,6 +64,12 @@ void QtHttpServer::start (quint16 port) {
 void QtHttpServer::stop (void) {
     if (m_sockServer->isListening ()) {
         m_sockServer->close ();
+		// disconnect clients
+		const QList<QTcpSocket*> socks = m_socksClientsHash.keys();
+		for(auto sock : socks)
+		{
+			sock->close();
+		}
         emit stopped ();
     }
 }
