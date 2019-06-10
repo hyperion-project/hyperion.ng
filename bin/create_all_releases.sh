ba3762e8658620e87bb55b93f774c6cae6f2f622
LordGrey <lordgrey.emmel@gmail.com>
2019-06-10 15:19:08 +0200

    Allow Cross-Compile with latest Ubuntu and Raspbian versions

--- bin/create_all_releases.sh
+++ bin/create_all_releases.sh
@@ -10,25 +10,31 @@ make_release()
 	PLATFORM=$2
 	shift 2
 
+	rm -r build-${RELEASE}
 	mkdir -p build-${RELEASE}
+	rm -r deploy/${RELEASE}
 	mkdir -p deploy/${RELEASE}
+
 	cd  build-${RELEASE}
 	cmake -DCMAKE_INSTALL_PREFIX=/usr -DPLATFORM=${PLATFORM} $@ -DCMAKE_BUILD_TYPE=Release -Wno-dev .. || exit 1
 	make -j $(nproc)  || exit 1
 	#strip bin/*
 	make package -j $(nproc)
-	mv hyperion-*-ambilight.* ../deploy/${RELEASE}
+	mv Hyperion-*.* ../deploy/${RELEASE}
 	cd ..
 	bin/create_release.sh . ${RELEASE}
 }
 
-export PATH="$PATH:$HOME/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin"
 CMAKE_PROTOC_FLAG="-DIMPORT_PROTOC=../build-x86x64/protoc_export.cmake"
+CMAKE_FLATC_FLAG="-DIMPORT_FLATC=../build-x86x64/flatc_export.cmake"
+
+export CFLAGS=-m32
+export CXXFLAGS=-m32
 
 make_release x86x64  x86
-#make_release x32     x86 ${CMAKE_PROTOC_FLAG}
-make_release rpi     rpi  -DCMAKE_TOOLCHAIN_FILE="../cmake/Toolchain-rpi.cmake" ${CMAKE_PROTOC_FLAG}
-make_release wetek   wetek  -DCMAKE_TOOLCHAIN_FILE="../cmake/Toolchain-rpi.cmake" ${CMAKE_PROTOC_FLAG}
-#make_release imx6   imx6 -DCMAKE_TOOLCHAIN_FILE="../cmake/Toolchain-imx6.cmake" ${CMAKE_PROTOC_FLAG}
+#make_release x32     x86   -DCMAKE_TOOLCHAIN_FILE="../cmake/Toolchain-x32.cmake" ${CMAKE_PROTOC_FLAG} ${CMAKE_FLATC_FLAG}
+make_release rpi     rpi   -DCMAKE_TOOLCHAIN_FILE="../cmake/Toolchain-rpi.cmake"  ${CMAKE_PROTOC_FLAG} ${CMAKE_FLATC_FLAG}
+#make_release wetek   wetek -DCMAKE_TOOLCHAIN_FILE="../cmake/Toolchain-rpi.cmake"  ${CMAKE_PROTOC_FLAG} ${CMAKE_FLATC_FLAG} 
+#make_release imx6    imx6  -DCMAKE_TOOLCHAIN_FILE="../cmake/Toolchain-imx6.cmake" ${CMAKE_PROTOC_FLAG} ${CMAKE_FLATC_FLAG} 
 
 
