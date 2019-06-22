b2c63ff770f92eeaff8f34e94f041057270f7080
Paulchen Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2019-06-22 16:12:09 +0200

    Pre-Release detection on Azure

--- test/jsonchecks/version.py
+++ test/jsonchecks/version.py
@@ -4,6 +4,11 @@ import json, sys
 retval   = 0
 
 with open(sys.argv[1]) as f:
-	data = json.load(f)
-	sys.stdout.write(data['versionnr'])
-	sys.exit(0)
+	if len(sys.argv) < 3:
+		data = json.load(f)
+		sys.stdout.write(data['versionnr'])
+		sys.exit(0)
+	else:
+		data = json.load(f)
+		sys.stdout.write(data['channel'])
+		sys.exit(0)
