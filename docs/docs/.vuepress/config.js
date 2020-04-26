5ace6cf58bb9210b94cf5fa74daa00fcc5ecca44
Sersch <edeltraud70@gmx.de>
2020-04-27 00:17:02 +0200

    resolve c&p issue

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -6,25 +6,25 @@ module.exports = {
     ['link', { rel: 'icon', type: 'image/png', sizes: '128x128', href: '/icons/favicon-128x128.png' }],
     ['link', { rel: 'icon', type: 'image/ico', href: '/icons/favicon.ico' }],
     // iPhone XR
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)", href="/icons/apple-launch-828x1792.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)", href: "/icons/apple-launch-828x1792.png" }],
     // iPhone X, XS
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)", href="/icons/apple-launch-1125x2436.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)", href: "/icons/apple-launch-1125x2436.png" }],
     // iPhone XS Max
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)", href="/icons/apple-launch-1242x2688.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)", href: "/icons/apple-launch-1242x2688.png" }],
     // iPhone 8, 7, 6s, 6
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", href="/icons/apple-launch-750x1334.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", href: "/icons/apple-launch-750x1334.png" }],
     // iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)", href ="/icons/apple-launch-1242x2208.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)", href: "/icons/apple-launch-1242x2208.png" }],
     // iPhone 5
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)", href ="/icons/apple-launch-640x1136.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)", href: "/icons/apple-launch-640x1136.png" }],
     // iPad Mini, Air, 9.7
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)", href="/icons/apple-launch-1536x2048.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)", href: "/icons/apple-launch-1536x2048.png" }],
     // iPad Pro 10.5
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)", href="/icons/apple-launch-1668x2224.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)", href: "/icons/apple-launch-1668x2224.png" }],
     // iPad Pro 11
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)", href="/icons/apple-launch-1668x2388.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)", href: "/icons/apple-launch-1668x2388.png" }],
     // iPad Pro 12.9"
-    ['link', { rel="apple-touch-startup-image", media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)", href="/icons/apple-launch-2048x2732.png" }],
+    ['link', { rel: "apple-touch-startup-image", media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)", href: "/icons/apple-launch-2048x2732.png" }],
 
     // PWA
     ['link', { rel: 'manifest', href: '/manifest.json' }],
