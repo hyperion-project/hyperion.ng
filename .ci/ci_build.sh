6b9c1a593b7f703fa3703e6e675989025bb786e9
Paulchen Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2020-05-03 20:21:33 +0200

    reduce/hide banner/copyright/log message
    Infos here: https://docs.microsoft.com/de-de/visualstudio/msbuild/msbuild-command-line-reference?view=vs-2019#switches

--- .ci/ci_build.sh
+++ .ci/ci_build.sh
@@ -42,7 +42,7 @@ elif [[ $CI_NAME == *"mingw64_nt"* || "$CI_NAME" == 'windows_nt' ]]; then
 	mkdir build || exit 1
 	cd build
 	cmake -G "Visual Studio 16 2019" -A x64 -DPLATFORM=${PLATFORM} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ../ || exit 2
-	cmake --build . --target package --config Release -- -maxcpucount || exit 3
+	cmake --build . --target package --config Release -- -nologo -v:m -maxcpucount || exit 3
 	exit 0;
 	exit 1 || { echo "---> Hyperion compilation failed! Abort"; exit 5; }
 elif [[ "$CI_NAME" == 'linux' ]]; then
