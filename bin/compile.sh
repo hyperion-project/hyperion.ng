b7a9a10223e6029678e8c0c7dc2ae4f849177bf5
Murat Seker <m.seker@aol.com>
2020-08-02 22:37:52 +0200

    refactor: Several random fixes + Experimental playground (#917)

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,24 +1,46 @@
 #!/bin/sh
 
+# exit on error
+set -e
+
 TARGET=${1:-hyperion}
 CFG="${2:-Release}"
 INST="$( [ "${3:-}" = "install" ] && echo true || echo false )"
 
 sudo apt-get update
-sudo apt-get install git cmake build-essential qtbase5-dev libqt5serialport5-dev libusb-1.0-0-dev libturbojpeg0-dev python3-dev libcec-dev libxcb-util0-dev libxcb-randr0-dev libxrandr-dev libxrender-dev libavahi-core-dev libavahi-compat-libdnssd-dev libssl-dev libjpeg-dev libqt5sql5-sqlite zlib1g-dev || exit 1
+sudo apt-get install                 \
+	git                          \
+	cmake                        \
+	build-essential              \
+	qtbase5-dev                  \
+	libqt5serialport5-dev        \
+	libusb-1.0-0-dev             \
+	libturbojpeg0-dev            \
+	python3-dev                  \
+	libcec-dev                   \
+	libxcb-util0-dev             \
+	libxcb-randr0-dev            \
+	libxrandr-dev                \
+	libxrender-dev               \
+	libavahi-core-dev            \
+	libavahi-compat-libdnssd-dev \
+	libssl-dev                   \
+	libjpeg-dev                  \
+	libqt5sql5-sqlite            \
+	zlib1g-dev                   \
 
 if [ -e /dev/vc-cma -a -e /dev/vc-mem ]
 then
-	sudo apt-get install libraspberrypi-dev || exit 1
+	sudo apt-get install libraspberrypi-dev
 fi
 
-git clone --recursive https://github.com/hyperion-project/hyperion.ng.git ${TARGET} || exit 1
+git clone --recursive https://github.com/hyperion-project/hyperion.ng.git ${TARGET}
 
-rm -rf  $TARGET/build
-mkdir -p $TARGET/build           || exit 1
-cd $TARGET/build                 || exit 1
-cmake -DCMAKE_BUILD_TYPE=$CFG .. || exit 1
-make -j $(nproc)                 || exit 1
+rm -rf $TARGET/build
+mkdir -p $TARGET/build
+cd $TARGET/build
+cmake -DCMAKE_BUILD_TYPE=$CFG ..
+make -j $(nproc)
 
 # optional: install into your system
 $INST && sudo make install/strip
