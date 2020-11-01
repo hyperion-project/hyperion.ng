c603cac02eec6f335a226fb11cabfece374b8e3b
LordGrey <lordgrey.emmel@gmail.com>
2020-11-01 10:56:20 +0100

    Fix config during start-up for missing elements

--- libsrc/hyperion/SettingsManager.cpp
+++ libsrc/hyperion/SettingsManager.cpp
@@ -99,7 +99,7 @@ SettingsManager::SettingsManager(quint8 instance, QObject* parent)
 		for (auto & schemaError : schemaChecker.getMessages())
 			Warning(_log, "Config Fix: %s", QSTRING_CSTR(schemaError));
 
-		saveSettings(dbConfig);
+		saveSettings(dbConfig,true);
 	}
 	else
 		_qconfig = dbConfig;
