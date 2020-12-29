a2e39687a59369865114d6892a4f72bfb0eeda6d
LordGrey <lordgrey.emmel@gmail.com>
2020-12-29 09:45:41 +0100

    Correct Grabber Namings

--- libsrc/grabber/directx/DirectXWrapper.cpp
+++ libsrc/grabber/directx/DirectXWrapper.cpp
@@ -1,7 +1,7 @@
 #include <grabber/DirectXWrapper.h>
 
 DirectXWrapper::DirectXWrapper(int cropLeft, int cropRight, int cropTop, int cropBottom, int pixelDecimation, int display, const unsigned updateRate_Hz)
-	: GrabberWrapper("DirectX", &_grabber, 0, 0, updateRate_Hz)
+	: GrabberWrapper("DX", &_grabber, 0, 0, updateRate_Hz)
 	, _grabber(cropLeft, cropRight, cropTop, cropBottom, pixelDecimation, display)
 {}
 
--- libsrc/grabber/osx/OsxWrapper.cpp
+++ libsrc/grabber/osx/OsxWrapper.cpp
@@ -1,7 +1,7 @@
 #include <grabber/OsxWrapper.h>
 
 OsxWrapper::OsxWrapper(unsigned display, unsigned grabWidth, unsigned grabHeight, unsigned updateRate_Hz)
-	: GrabberWrapper("OSX FrameGrabber", &_grabber, grabWidth, grabHeight, updateRate_Hz)
+	: GrabberWrapper("OSX", &_grabber, grabWidth, grabHeight, updateRate_Hz)
 	, _grabber(display, grabWidth, grabHeight)
 {}
 
