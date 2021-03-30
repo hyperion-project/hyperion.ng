e20fca5c453242c8797add7bde4d8f0ac54dbc0a
LordGrey <lordgrey.emmel@gmail.com>
2021-03-30 23:35:52 +0200

    Logging - Fix autoscroll and optimizations

--- assets/webconfig/js/ui_utils.js
+++ assets/webconfig/js/ui_utils.js
@@ -865,6 +865,43 @@ function getReleases(callback) {
   });
 }
 
+function getSystemInfo() {
+  var sys = window.sysInfo.system;
+  var shy = window.sysInfo.hyperion;
+
+  var info = "Hyperion Server: \n";
+  info += '- Build:           ' + shy.build + '\n';
+  info += '- Build time:      ' + shy.time + '\n';
+  info += '- Git Remote:      ' + shy.gitremote + '\n';
+  info += '- Version:         ' + shy.version + '\n';
+  info += '- UI Lang:         ' + storedLang + ' (BrowserLang: ' + navigator.language + ')\n';
+  info += '- UI Access:       ' + storedAccess + '\n';
+  //info += '- Log lvl:         ' + window.serverConfig.logger.level + '\n';
+  info += '- Avail Capt:      ' + window.serverInfo.grabbers.available + '\n';
+  info += '- Database:        ' + (shy.readOnlyMode ? "ready-only" : "read/write") + '\n';
+
+  info += '\n';
+
+  info += 'Hyperion Server OS: \n';
+  info += '- Distribution:   ' + sys.prettyName + '\n';
+  info += '- Architecture:   ' + sys.architecture + '\n';
+
+  if (sys.cpuModelName)
+    info += '- CPU Model:      ' + sys.cpuModelName + '\n';
+  if (sys.cpuModelType)
+    info += '- CPU Type:       ' + sys.cpuModelType + '\n';
+  if (sys.cpuRevision)
+    info += '- CPU Revision:   ' + sys.cpuRevision + '\n';
+  if (sys.cpuHardware)
+    info += '- CPU Hardware:   ' + sys.cpuHardware + '\n';
+
+  info += '- Kernel:         ' + sys.kernelType + ' (' + sys.kernelVersion + ' (WS: ' + sys.wordSize + '))\n';
+  info += '- Qt Version:     ' + sys.qtVersion + '\n';
+  info += '- Python Version: ' + sys.pyVersion + '\n';
+  info += '- Browser:        ' + navigator.userAgent;
+  return info;
+}
+
 function handleDarkMode() {
   $("<link/>", {
     rel: "stylesheet",
