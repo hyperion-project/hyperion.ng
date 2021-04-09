a126276880bc87604b0eadcf58883e3e3bebf888
LordGrey <lordgrey.emmel@gmail.com>
2021-04-09 19:47:47 +0200

    Stop background effect, when out of scope

--- include/hyperion/BGEffectHandler.h
+++ include/hyperion/BGEffectHandler.h
@@ -4,6 +4,7 @@
 #include <hyperion/Hyperion.h>
 #include <utils/settings.h>
 #include <effectengine/Effect.h>
+#include <hyperion/PriorityMuxer.h>
 
 ///
 /// @brief Handle the background Effect settings, reacts on runtime to settings changes
@@ -14,11 +15,16 @@ class BGEffectHandler : public QObject
 
 public:
 	BGEffectHandler(Hyperion* hyperion)
-	: QObject(hyperion)
-	, _hyperion(hyperion)
+		: QObject(hyperion)
+		, _hyperion(hyperion)
+		, _prioMuxer(nullptr)
+		, _isBgEffectConfigured(false)
 	{
 		// listen for config changes
 		connect(_hyperion, &Hyperion::settingsChanged, this, &BGEffectHandler::handleSettingsUpdate);
+
+		_prioMuxer = _hyperion->getMuxerInstance();
+		connect(_prioMuxer,&PriorityMuxer::prioritiesChanged, this, &BGEffectHandler::handlePriorityUpdate);
 
 		// initialization
 		handleSettingsUpdate(settings::BGEFFECT, _hyperion->getSetting(settings::BGEFFECT));
@@ -34,14 +40,18 @@ private slots:
 	{
 		if(type == settings::BGEFFECT)
 		{
-			const QJsonObject& BGEffectConfig = config.object();
+			_isBgEffectConfigured = false;
+			_bgEffectConfig = config;
 
+			const QJsonObject& BGEffectConfig = _bgEffectConfig.object();
 			#define BGCONFIG_ARRAY bgColorConfig.toArray()
 			// clear background priority
 			_hyperion->clear(PriorityMuxer::BG_PRIORITY);
 			// initial background effect/color
 			if (BGEffectConfig["enable"].toBool(true))
 			{
+				_isBgEffectConfigured = true;
+
 				const QString bgTypeConfig = BGEffectConfig["type"].toString("effect");
 				const QString bgEffectConfig = BGEffectConfig["effect"].toString("Warm mood blobs");
 				const QJsonValue bgColorConfig = BGEffectConfig["color"];
@@ -63,12 +73,36 @@ private slots:
 					Info(Logger::getInstance("HYPERION"),"Initial background effect '%s' %s", QSTRING_CSTR(bgEffectConfig), ((result == 0) ? "started" : "failed"));
 				}
 			}
-
 			#undef BGCONFIG_ARRAY
+		}
+	}
+
+	///
+	/// @brief Handle priority updates.
+	/// In case the background effect is not current priority, stop BG-effect to save resources; otherwise start effect again.
+	///
+	void handlePriorityUpdate()
+	{
+		if (_prioMuxer->getCurrentPriority() != PriorityMuxer::BG_PRIORITY && _prioMuxer->hasPriority(PriorityMuxer::BG_PRIORITY))
+		{
+			Debug(Logger::getInstance("HYPERION"),"Stop background (color-) effect as it moved out of scope");
+			_hyperion->clear(PriorityMuxer::BG_PRIORITY);
+		}
+		else
+		{
+			if (_prioMuxer->getCurrentPriority() == PriorityMuxer::LOWEST_PRIORITY && _isBgEffectConfigured)
+			{
+				emit handleSettingsUpdate (settings::BGEFFECT, _bgEffectConfig);
+			}
 		}
 	}
 
 private:
 	/// Hyperion instance pointer
 	Hyperion* _hyperion;
+	/// priority muxer instance
+	PriorityMuxer* _prioMuxer;
+
+	QJsonDocument _bgEffectConfig;
+	bool _isBgEffectConfigured;
 };
