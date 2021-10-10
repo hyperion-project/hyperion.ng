cf5b4cbe8d81eeb03cee3e9fedf67260980a4124
Markus <16664240+Paulchen-Panther@users.noreply.github.com>
2021-10-10 12:31:56 +0000

    Apple macOS DragNDrop implementation - Trying again to fix Issue #1109 - integrate 3rd Party Lincense into embedded webconfig

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,8 +1,9 @@
 # cmake file for generating distribution packages
 
-# default packages to build
+# Default packages to build
 IF (APPLE)
-	SET ( CPACK_GENERATOR "TGZ")
+	SET ( CPACK_GENERATOR "DragNDrop")
+	SET ( CPACK_DMG_FORMAT "UDBZ" )
 ELSEIF (UNIX)
 	SET ( CPACK_GENERATOR "TGZ")
 ELSEIF (WIN32)
@@ -11,11 +12,12 @@ ENDIF()
 
 # Determine packages by found generator executables
 find_package(RpmBuilder)
-find_package(DebBuilder)
 IF(RPM_BUILDER_FOUND)
 	message(STATUS "CPACK: Found RPM builder")
 	SET ( CPACK_GENERATOR ${CPACK_GENERATOR} "RPM")
 ENDIF()
+
+find_package(DebBuilder)
 IF(DEB_BUILDER_FOUND)
 	message(STATUS "CPACK: Found DEB builder")
 	SET ( CPACK_GENERATOR ${CPACK_GENERATOR} "DEB")
@@ -43,12 +45,12 @@ SET ( CPACK_PACKAGE_CONTACT "packages@hyperion-project.org")
 SET ( CPACK_PACKAGE_VENDOR "hyperion-project")
 SET ( CPACK_PACKAGE_EXECUTABLES "hyperiond;Hyperion" )
 SET ( CPACK_PACKAGE_INSTALL_DIRECTORY "Hyperion" )
-SET ( CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/resources/icons/hyperion-icon-32px.png")
+SET ( CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/resources/icons/hyperion-icon-32px.png" )
 
 SET ( CPACK_PACKAGE_VERSION_MAJOR "${HYPERION_VERSION_MAJOR}")
 SET ( CPACK_PACKAGE_VERSION_MINOR "${HYPERION_VERSION_MINOR}")
 SET ( CPACK_PACKAGE_VERSION_PATCH "${HYPERION_VERSION_PATCH}")
-SET ( CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE" )
+SET ( CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE" )
 SET ( CPACK_PACKAGE_EXECUTABLES "hyperiond;Hyperion" )
 SET ( CPACK_CREATE_DESKTOP_LINKS "hyperiond;Hyperion" )
 
@@ -57,46 +59,46 @@ if (NOT "${HYPERION_VERSION_PRE}" STREQUAL "")
 	string(APPEND CPACK_PACKAGE_VERSION_PATCH ${HYPERION_VERSION_PRE})
 endif()
 
-# Define the install prefix path for cpack
-IF ( UNIX )
-	#SET ( CPACK_PACKAGING_INSTALL_PREFIX "share/hyperion")
-ENDIF()
-
 # Specific CPack Package Generators
-# https://cmake.org/Wiki/CMake:CPackPackageGenerators
+# https://gitlab.kitware.com/cmake/community/-/wikis/doc/cpack/PackageGenerators
+
 # .deb files for apt
-SET ( CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CMAKE_CURRENT_SOURCE_DIR}/cmake/package-scripts/preinst;${CMAKE_CURRENT_SOURCE_DIR}/cmake/package-scripts/postinst;${CMAKE_CURRENT_SOURCE_DIR}/cmake/package-scripts/prerm" )
+# https://cmake.org/cmake/help/latest/cpack_gen/deb.html
+SET ( CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CMAKE_SOURCE_DIR}/cmake/debian/preinst;${CMAKE_SOURCE_DIR}/cmake/debian/postinst;${CMAKE_SOURCE_DIR}/cmake/debian/prerm" )
 SET ( CPACK_DEBIAN_PACKAGE_DEPENDS "libcec6 | libcec4" )
 SET ( CPACK_DEBIAN_PACKAGE_SECTION "Miscellaneous" )
 
 # .rpm for rpm
-# https://cmake.org/cmake/help/v3.5/module/CPackRPM.html
+# https://cmake.org/cmake/help/latest/cpack_gen/rpm.html
 SET ( CPACK_RPM_PACKAGE_RELEASE 1 )
 SET ( CPACK_RPM_PACKAGE_LICENSE "MIT" )
 SET ( CPACK_RPM_PACKAGE_GROUP "Applications" )
 SET ( CPACK_RPM_PACKAGE_REQUIRES "libcec >= 4.0.0" )
-SET ( CPACK_RPM_PRE_INSTALL_SCRIPT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/cmake/package-scripts/preinst" )
-SET ( CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/cmake/package-scripts/postinst" )
-SET ( CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/cmake/package-scripts/prerm" )
+SET ( CPACK_RPM_PRE_INSTALL_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/cmake/rpm/preinst" )
+SET ( CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/cmake/rpm/postinst" )
+SET ( CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/cmake/rpm/prerm" )
 
-# OSX "Bundle" generator TODO Add more osx generators
-# https://cmake.org/cmake/help/v3.10/module/CPackBundle.html
-SET ( CPACK_BUNDLE_NAME "Hyperion" )
-SET ( CPACK_BUNDLE_ICON ${CMAKE_CURRENT_SOURCE_DIR}/cmake/osxbundle/Hyperion.icns )
-SET ( CPACK_BUNDLE_PLIST ${CMAKE_CURRENT_SOURCE_DIR}/cmake/osxbundle/Info.plist )
-SET ( CPACK_BUNDLE_STARTUP_COMMAND "${CMAKE_SOURCE_DIR}/cmake/osxbundle/launch.sh" )
+# .dmg for Apple macOS
+# https://cmake.org/cmake/help/latest/cpack_gen/dmg.html
+IF (APPLE)
+	SET ( CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/cmake/osxbundle/PackageIcon.icns" )
+	SET ( CPACK_RESOURCE_FILE_LICENSE "${CMAKE_BINARY_DIR}/LICENSE" )
+	SET ( CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/cmake/osxbundle/Background.png" )
+	SET ( CPACK_DMG_DS_STORE_SETUP_SCRIPT "${CMAKE_SOURCE_DIR}/cmake/osxbundle/AppleScript.scpt" )
+ENDIF(APPLE)
 
 # Windows NSIS
 # Use custom script based on cpack nsis template
 set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/nsis/template ${CMAKE_MODULE_PATH})
+
 # Some path transformations
 if(WIN32)
 	file(TO_NATIVE_PATH ${CPACK_PACKAGE_ICON} CPACK_PACKAGE_ICON)
 	STRING(REGEX REPLACE "\\\\" "\\\\\\\\" CPACK_PACKAGE_ICON ${CPACK_PACKAGE_ICON})
 endif()
-file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/nsis/installer.ico" NSIS_HYP_ICO)
-file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/nsis/header.bmp" NSIS_HYP_LOGO_HORI)
-file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/nsis/logo.bmp" NSIS_HYP_LOGO_VERT)
+file(TO_NATIVE_PATH "${CMAKE_SOURCE_DIR}/cmake/nsis/installer.ico" NSIS_HYP_ICO)
+file(TO_NATIVE_PATH "${CMAKE_SOURCE_DIR}/cmake/nsis/header.bmp" NSIS_HYP_LOGO_HORI)
+file(TO_NATIVE_PATH "${CMAKE_SOURCE_DIR}/cmake/nsis/logo.bmp" NSIS_HYP_LOGO_VERT)
 STRING(REGEX REPLACE "\\\\" "\\\\\\\\" NSIS_HYP_ICO "${NSIS_HYP_ICO}")
 STRING(REGEX REPLACE "\\\\" "\\\\\\\\" NSIS_HYP_LOGO_VERT "${NSIS_HYP_LOGO_VERT}")
 STRING(REGEX REPLACE "\\\\" "\\\\\\\\" NSIS_HYP_LOGO_HORI "${NSIS_HYP_LOGO_HORI}")
@@ -113,19 +115,25 @@ SET ( CPACK_NSIS_HELP_LINK "https://www.hyperion-project.org")
 SET ( CPACK_NSIS_URL_INFO_ABOUT "https://www.hyperion-project.org")
 SET ( CPACK_NSIS_MUI_FINISHPAGE_RUN "hyperiond.exe")
 SET ( CPACK_NSIS_BRANDING_TEXT "Hyperion-${HYPERION_VERSION}")
+
 # custom nsis plugin directory
 SET ( CPACK_NSIS_EXTRA_DEFS "!addplugindir ${CMAKE_SOURCE_DIR}/cmake/nsis/plugins")
+
 # additional hyperiond startmenu link, won't be created if the user disables startmenu links
 SET ( CPACK_NSIS_CREATE_ICONS_EXTRA "CreateShortCut '$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\Hyperion (Console).lnk' '$INSTDIR\\\\bin\\\\hyperiond.exe' '-d -c'")
 SET ( CPACK_NSIS_DELETE_ICONS_EXTRA "Delete '$SMPROGRAMS\\\\$MUI_TEMP\\\\Hyperion (Console).lnk'")
 
-# define the install components
+# Define the install components
 # See also https://gitlab.kitware.com/cmake/community/-/wikis/doc/cpack/Component-Install-With-CPack
 # and https://cmake.org/cmake/help/latest/module/CPackComponent.html
 SET ( CPACK_COMPONENTS_GROUPING "ALL_COMPONENTS_IN_ONE")
-# Components base
-SET ( CPACK_COMPONENTS_ALL "Hyperion" "hyperion_remote" )
-# optional compiled
+
+# Components base (All builds)
+SET ( CPACK_COMPONENTS_ALL "Hyperion" )
+
+SET ( CPACK_COMPONENTS_ALL ${CPACK_COMPONENTS_ALL} "hyperion_remote" )
+
+# Optional compiled
 if(ENABLE_QT)
 	SET ( CPACK_COMPONENTS_ALL ${CPACK_COMPONENTS_ALL} "hyperion_qt" )
 endif()
@@ -151,6 +159,11 @@ if(ENABLE_OSX)
 	SET ( CPACK_COMPONENTS_ALL ${CPACK_COMPONENTS_ALL} "hyperion_osx" )
 endif()
 
+# Only include Hyperion to macOS dmg package (without standalone programs)
+IF ( CPACK_GENERATOR MATCHES "DragNDrop" )
+	LIST ( REMOVE_ITEM CPACK_COMPONENTS_ALL "hyperion_remote" "hyperion_qt" "hyperion_osx" )
+ENDIF()
+
 SET ( CPACK_ARCHIVE_COMPONENT_INSTALL ON )
 SET ( CPACK_DEB_COMPONENT_INSTALL ON )
 SET ( CPACK_RPM_COMPONENT_INSTALL ON )
@@ -164,6 +177,7 @@ cpack_add_install_type(Full DISPLAY_NAME "Full")
 cpack_add_install_type(Min DISPLAY_NAME "Minimal")
 cpack_add_component_group(Runtime EXPANDED DESCRIPTION "Hyperion runtime and hyperion-remote commandline tool")
 cpack_add_component_group(Screencapture EXPANDED DESCRIPTION "Standalone Screencapture commandline programs")
+
 # Components base
 cpack_add_component(Hyperion
 	DISPLAY_NAME "Hyperion"
@@ -172,6 +186,7 @@ cpack_add_component(Hyperion
 	GROUP Runtime
 	REQUIRED
 )
+
 cpack_add_component(hyperion_remote
 	DISPLAY_NAME "Hyperion Remote"
 	DESCRIPTION "Hyperion remote cli tool"
@@ -253,4 +268,3 @@ if(ENABLE_OSX)
 		DEPENDS Hyperion
 	)
 endif()
-
