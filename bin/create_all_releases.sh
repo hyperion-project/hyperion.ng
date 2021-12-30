2e6993baf324e91a53dc703a2a362ff4da8f54a2
LordGrey <lordgrey.emmel@gmail.com>
2021-12-30 19:15:37 +0100

    Correct CMake files

--- bin/create_all_releases.sh
+++ bin/create_all_releases.sh
@@ -28,7 +28,7 @@ make_release()
 }
 
 #export QTDIR="/opt/Qt/6.2.0/gcc_64"
-#export QTDIR=="/opt/Qt/5.15.2/gcc_64"
+#export QTDIR="/opt/Qt/5.15.2/gcc_64"
 
 CMAKE_PROTOC_FLAG="-DIMPORT_PROTOC=../build-x86x64/protoc_export.cmake"
 CMAKE_FLATC_FLAG="-DIMPORT_FLATC=../build-x86x64/flatc_export.cmake"
