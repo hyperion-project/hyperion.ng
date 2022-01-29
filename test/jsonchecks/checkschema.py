eb9608da033ecf4586edf040404bb6ec45a426d8
LordGrey <lordgrey.emmel@gmail.com>
2022-01-29 17:28:17 +0100

    Fix file uri generation

--- test/jsonchecks/checkschema.py
+++ test/jsonchecks/checkschema.py
@@ -11,7 +11,7 @@ schemaFileName = sys.argv[2]
 try:
 	with open(schemaFileName) as schemaFile:
 		with open(jsonFileName) as jsonFile:
-			resolver = RefResolver('file://%s/schema/' % path.abspath(path.dirname(schemaFileName)), None)
+			resolver = RefResolver('file:///%s/schema/' % path.abspath(path.dirname(schemaFileName)), None)
 			Draft3Validator(json.loads(schemaFile.read()), resolver=resolver).validate(json.loads(jsonFile.read()))
 except Exception as e:
 	print('validation error: '+jsonFileName + ' '+schemaFileName+' ('+str(e)+')')
