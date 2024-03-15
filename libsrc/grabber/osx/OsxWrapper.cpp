2284d2e45c3686e21e0a956778031fb2cb71f05d
LordGrey <48840279+Lord-Grey@users.noreply.github.com>
2024-03-15 17:07:21 +0100

    Fix #1714

--- libsrc/grabber/osx/OsxWrapper.cpp
+++ libsrc/grabber/osx/OsxWrapper.cpp
@@ -5,13 +5,15 @@ OsxWrapper::OsxWrapper( int updateRate_Hz,
 						int pixelDecimation
 						)
 	: GrabberWrapper(GRABBERTYPE, &_grabber, updateRate_Hz)
-	  , _grabber(display)
+	, _grabber(display)
 {
 	_grabber.setPixelDecimation(pixelDecimation);
 }
 
 OsxWrapper::OsxWrapper(const QJsonDocument& grabberConfig)
-	: GrabberWrapper(GRABBERTYPE, &_grabber)
+	: OsxWrapper(GrabberWrapper::DEFAULT_RATE_HZ,
+				 kCGDirectMainDisplay,
+				 GrabberWrapper::DEFAULT_PIXELDECIMATION)
 {
 	this->handleSettingsUpdate(settings::SYSTEMCAPTURE, grabberConfig);
 }
