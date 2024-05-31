76fff98f5cc1f20c2c3e059bf9ded9ee73d564e1
LordGrey <48840279+Lord-Grey@users.noreply.github.com>
2024-05-31 23:08:13 +0200

    Implement ftdi led device - 2 (#1746)

--- doc/development/CompileHowto.md
+++ doc/development/CompileHowto.md
@@ -61,14 +61,14 @@ cd $HYPERION_HOME
 
 ```console
 sudo apt-get update
-sudo apt-get install git cmake build-essential qtbase5-dev libqt5serialport5-dev libqt5sql5-sqlite libqt5svg5-dev libqt5x11extras5-dev libusb-1.0-0-dev python3-dev libasound2-dev libturbojpeg0-dev libjpeg-dev libssl-dev
+sudo apt-get install git cmake build-essential qtbase5-dev libqt5serialport5-dev libqt5sql5-sqlite libqt5svg5-dev libqt5x11extras5-dev libusb-1.0-0-dev python3-dev libasound2-dev libturbojpeg0-dev libjpeg-dev libssl-dev libftdi1-dev
 ```
 
 **Ubuntu (22.04+) - Qt6 based**
 
 ```console
 sudo apt-get update
-sudo apt-get install git cmake build-essential qt6-base-dev libqt6serialport6-dev libxkbcommon-dev libvulkan-dev libgl1-mesa-dev libusb-1.0-0-dev python3-dev libasound2-dev libturbojpeg0-dev libjpeg-dev libssl-dev pkg-config
+sudo apt-get install git cmake build-essential qt6-base-dev libqt6serialport6-dev libxkbcommon-dev libvulkan-dev libgl1-mesa-dev libusb-1.0-0-dev python3-dev libasound2-dev libturbojpeg0-dev libjpeg-dev libssl-dev pkg-config libftdi1-dev
 ```
 
 **For Linux X11/XCB grabber support**
@@ -110,7 +110,7 @@ See [AUR](https://aur.archlinux.org/packages/?O=0&SeB=nd&K=hyperion&outdated=&SB
 The following dependencies are needed to build hyperion.ng on fedora.
 ```console
 sudo dnf -y groupinstall "Development Tools"
-sudo dnf install python3-devel qt-devel qt5-qtbase-devel qt5-qtserialport-devel xrandr xcb-util-image-devel qt5-qtx11extras-devel alsa-lib-devel turbojpeg-devel libusb-devel xcb-util-devel dbus-devel openssl-devel fedora-packager rpmdevtools gcc libcec-devel
+sudo dnf install python3-devel qt-devel qt5-qtbase-devel qt5-qtserialport-devel xrandr xcb-util-image-devel qt5-qtx11extras-devel alsa-lib-devel turbojpeg-devel libusb-devel xcb-util-devel dbus-devel openssl-devel fedora-packager rpmdevtools gcc libcec-devel libftdi1-dev
 ```
 After installing the dependencies, you can continue with the compile instructions later on this page (the more detailed way..).
 
@@ -119,7 +119,7 @@ To install on OS X you either need [Homebrew](https://brew.sh/) or [Macport](htt
 
 First you need to install the dependencies:
 ```console
-brew install git qt@5 python3 cmake libusb openssl@1.1
+brew install git qt@5 python3 cmake libusb openssl@1.1 libftdi
 ```
 
 ## Windows
--- libsrc/utils/RgbToRgbw.cpp
+++ libsrc/utils/RgbToRgbw.cpp
@@ -3,6 +3,8 @@
 #include <utils/RgbToRgbw.h>
 #include <utils/Logger.h>
 
+#define ROUND_DIVIDE(number, denom) (((number) + (denom) / 2) / (denom))
+
 namespace RGBW {
 
 WhiteAlgorithm stringToWhiteAlgorithm(const QString& str)
@@ -19,7 +21,27 @@ WhiteAlgorithm stringToWhiteAlgorithm(const QString& str)
 	{
 		return WhiteAlgorithm::SUB_MIN_COOL_ADJUST;
 	}
-	if (str.isEmpty() || str == "white_off")
+    if (str == "cold_white")
+    {
+        return WhiteAlgorithm::COLD_WHITE;
+    }
+    if (str == "neutral_white")
+    {
+        return WhiteAlgorithm::NEUTRAL_WHITE;
+    }
+    if (str == "auto")
+    {
+        return WhiteAlgorithm::AUTO;
+    }
+    if (str == "auto_max")
+    {
+        return WhiteAlgorithm::AUTO_MAX;
+    }
+    if (str == "auto_accurate")
+    {
+        return WhiteAlgorithm::AUTO_ACCURATE;
+    }
+    if (str.isEmpty() || str == "white_off")
 	{
 		return WhiteAlgorithm::WHITE_OFF;
 	}
@@ -77,6 +99,63 @@ void Rgb_to_Rgbw(ColorRgb input, ColorRgbw * output, WhiteAlgorithm algorithm)
 			output->white = 0;
 			break;
 		}
+
+        case WhiteAlgorithm::AUTO_MAX:
+        {
+            output->red = input.red;
+            output->green = input.green;
+            output->blue = input.blue;
+            output->white = input.red > input.green ? (input.red > input.blue ? input.red : input.blue) : (input.green > input.blue ? input.green : input.blue);
+            break;
+        }
+
+        case WhiteAlgorithm::AUTO_ACCURATE:
+        {
+            output->white = input.red < input.green ? (input.red < input.blue ? input.red : input.blue) : (input.green < input.blue ? input.green : input.blue);
+            output->red = input.red - output->white;
+            output->green = input.green - output->white;
+            output->blue = input.blue - output->white;
+            break;
+        }
+
+        case WhiteAlgorithm::AUTO:
+        {
+
+            output->red = input.red;
+            output->green = input.green;
+            output->blue = input.blue;
+            output->white = input.red < input.green ? (input.red < input.blue ? input.red : input.blue) : (input.green < input.blue ? input.green : input.blue);
+            break;
+        }
+        case WhiteAlgorithm::NEUTRAL_WHITE:
+        case WhiteAlgorithm::COLD_WHITE:
+        {
+            //cold white config
+            uint8_t gain = 0xFF;
+            uint8_t red = 0xA0;
+            uint8_t green = 0xA0;
+            uint8_t blue = 0xA0;
+
+            if (algorithm == WhiteAlgorithm::NEUTRAL_WHITE) {
+                gain = 0xFF;
+                red = 0xB0;
+                green = 0xB0;
+                blue = 0x70;
+            }
+
+            uint8_t _r = qMin((uint32_t)(ROUND_DIVIDE(red * input.red,  0xFF)), (uint32_t)0xFF);
+            uint8_t _g = qMin((uint32_t)(ROUND_DIVIDE(green * input.green,  0xFF)), (uint32_t)0xFF);
+            uint8_t _b = qMin((uint32_t)(ROUND_DIVIDE(blue * input.blue,  0xFF)), (uint32_t)0xFF);
+
+            output->white = qMin(_r, qMin(_g, _b));
+            output->red = input.red - _r;
+            output->green = input.green - _g;
+            output->blue = input.blue - _b;
+
+            uint8_t _w = qMin((uint32_t)(ROUND_DIVIDE(gain * output->white,  0xFF)), (uint32_t)0xFF);
+            output->white = _w;
+            break;
+        }
 		default:
 			break;
 	}
