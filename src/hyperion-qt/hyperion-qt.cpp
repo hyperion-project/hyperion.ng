975573ea383f7486d23084a61c6ad33a3a46055b
LordGrey <48840279+Lord-Grey@users.noreply.github.com>
2025-01-25 19:57:48 +0100

    Standalone Qt grabber - only capture when connected

--- src/hyperion-qt/hyperion-qt.cpp
+++ src/hyperion-qt/hyperion-qt.cpp
@@ -5,6 +5,8 @@
 #include "QtWrapper.h"
 #include <utils/ColorRgb.h>
 #include <utils/Image.h>
+#include <utils/DefaultSignalHandler.h>
+#include <utils/ErrorManager.h>
 
 #include "HyperionConfig.h"
 #include <commandline/Parser.h>
@@ -34,138 +36,155 @@ void saveScreenshot(const QString& filename, const Image<ColorRgb> & image)
 
 int main(int argc, char ** argv)
 {
+	DefaultSignalHandler::install();
+	ErrorManager errorManager;
+
 	Logger *log = Logger::getInstance("QTGRABBER");
 	Logger::setLogLevel(Logger::INFO);
 
 	std::cout
-		<< "hyperion-qt:" << std::endl
-		<< "\tVersion   : " << HYPERION_VERSION << " (" << HYPERION_BUILD_ID << ")" << std::endl
-		<< "\tbuild time: " << __DATE__ << " " << __TIME__ << std::endl;
+			<< "hyperion-qt:" << std::endl
+			<< "\tVersion   : " << HYPERION_VERSION << " (" << HYPERION_BUILD_ID << ")" << std::endl
+			<< "\tbuild time: " << __DATE__ << " " << __TIME__ << std::endl;
 
 	QGuiApplication app(argc, argv);
 
-	try
+	QObject::connect(&errorManager, &ErrorManager::errorOccurred, [&](QString error) {
+		Error(log, "Error occured %s", QSTRING_CSTR(error));
+		Logger::deleteInstance();
+		QCoreApplication::exit(-1);
+	});
+
+	// create the option parser and initialize all parameters
+	Parser parser("Qt interface capture application for Hyperion. Will automatically search a Hyperion server if -a option isn't used. Please note that if you have more than one server running it's more or less random which one will be used.");
+
+	IntOption      & argDisplay         = parser.add<IntOption>    ('d', "display",        "Set the display to capture [default: %1]", "0");
+
+	IntOption      & argFps             = parser.add<IntOption>    ('f', "framerate",      QString("Capture frame rate. Range %1-%2fps").arg(GrabberWrapper::DEFAULT_MIN_GRAB_RATE_HZ).arg(GrabberWrapper::DEFAULT_MAX_GRAB_RATE_HZ), QString::number(GrabberWrapper::DEFAULT_RATE_HZ), GrabberWrapper::DEFAULT_MIN_GRAB_RATE_HZ, GrabberWrapper::DEFAULT_MAX_GRAB_RATE_HZ);
+	IntOption      & argSizeDecimation	= parser.add<IntOption>    ('s', "size-decimator", "Decimation factor for the output image size [default=%1]", QString::number(GrabberWrapper::DEFAULT_PIXELDECIMATION), 1);
+
+	IntOption      & argCropLeft        = parser.add<IntOption>    (0x0, "crop-left",      "Number of pixels to crop from the left of the picture before decimation");
+	IntOption      & argCropRight       = parser.add<IntOption>    (0x0, "crop-right",     "Number of pixels to crop from the right of the picture before decimation");
+	IntOption      & argCropTop         = parser.add<IntOption>    (0x0, "crop-top",       "Number of pixels to crop from the top of the picture before decimation");
+	IntOption      & argCropBottom      = parser.add<IntOption>    (0x0, "crop-bottom",    "Number of pixels to crop from the bottom of the picture before decimation");
+	BooleanOption  & arg3DSBS			= parser.add<BooleanOption>(0x0, "3DSBS",          "Interpret the incoming video stream as 3D side-by-side");
+	BooleanOption  & arg3DTAB			= parser.add<BooleanOption>(0x0, "3DTAB",          "Interpret the incoming video stream as 3D top-and-bottom");
+
+	Option         & argAddress         = parser.add<Option>       ('a', "address",        "The hostname or IP-address (IPv4 or IPv6) of the hyperion server.\nDefault host: %1, port: 19400.\nSample addresses:\nHost : hyperion.fritz.box\nIPv4 : 127.0.0.1:19400\nIPv6 : [2001:1:2:3:4:5:6:7]", "127.0.0.1");
+	IntOption      & argPriority        = parser.add<IntOption>    ('p', "priority",       "Use the provided priority channel (suggested 100-199) [default: %1]", "150");
+	BooleanOption  & argSkipReply       = parser.add<BooleanOption>(0x0, "skip-reply",     "Do not receive and check reply messages from Hyperion");
+
+	BooleanOption  & argScreenshot      = parser.add<BooleanOption>(0x0, "screenshot",     "Take a single screenshot, save it to file and quit");
+
+	BooleanOption  & argDebug           = parser.add<BooleanOption>(0x0, "debug",          "Enable debug logging");
+	BooleanOption  & argHelp            = parser.add<BooleanOption>('h', "help",           "Show this help message and exit");
+
+	// parse all arguments
+	parser.process(app);
+
+	// check if debug logging is required
+	if (parser.isSet(argDebug))
 	{
-		// create the option parser and initialize all parameters
-		Parser parser("Qt interface capture application for Hyperion. Will automatically search a Hyperion server if -a option isn't used. Please note that if you have more than one server running it's more or less random which one will be used.");
-
-		IntOption      & argDisplay         = parser.add<IntOption>    ('d', "display",        "Set the display to capture [default: %1]", "0");
-
-		IntOption      & argFps             = parser.add<IntOption>    ('f', "framerate",      QString("Capture frame rate. Range %1-%2fps").arg(GrabberWrapper::DEFAULT_MIN_GRAB_RATE_HZ).arg(GrabberWrapper::DEFAULT_MAX_GRAB_RATE_HZ), QString::number(GrabberWrapper::DEFAULT_RATE_HZ), GrabberWrapper::DEFAULT_MIN_GRAB_RATE_HZ, GrabberWrapper::DEFAULT_MAX_GRAB_RATE_HZ);
-		IntOption      & argSizeDecimation	= parser.add<IntOption>    ('s', "size-decimator", "Decimation factor for the output image size [default=%1]", QString::number(GrabberWrapper::DEFAULT_PIXELDECIMATION), 1);
-
-		IntOption      & argCropLeft        = parser.add<IntOption>    (0x0, "crop-left",      "Number of pixels to crop from the left of the picture before decimation");
-		IntOption      & argCropRight       = parser.add<IntOption>    (0x0, "crop-right",     "Number of pixels to crop from the right of the picture before decimation");
-		IntOption      & argCropTop         = parser.add<IntOption>    (0x0, "crop-top",       "Number of pixels to crop from the top of the picture before decimation");
-		IntOption      & argCropBottom      = parser.add<IntOption>    (0x0, "crop-bottom",    "Number of pixels to crop from the bottom of the picture before decimation");
-		BooleanOption  & arg3DSBS			= parser.add<BooleanOption>(0x0, "3DSBS",          "Interpret the incoming video stream as 3D side-by-side");
-		BooleanOption  & arg3DTAB			= parser.add<BooleanOption>(0x0, "3DTAB",          "Interpret the incoming video stream as 3D top-and-bottom");
-
-		Option         & argAddress         = parser.add<Option>       ('a', "address",        "The hostname or IP-address (IPv4 or IPv6) of the hyperion server.\nDefault host: %1, port: 19400.\nSample addresses:\nHost : hyperion.fritz.box\nIPv4 : 127.0.0.1:19400\nIPv6 : [2001:1:2:3:4:5:6:7]", "127.0.0.1");
-		IntOption      & argPriority        = parser.add<IntOption>    ('p', "priority",       "Use the provided priority channel (suggested 100-199) [default: %1]", "150");
-		BooleanOption  & argSkipReply       = parser.add<BooleanOption>(0x0, "skip-reply",     "Do not receive and check reply messages from Hyperion");
-
-		BooleanOption  & argScreenshot      = parser.add<BooleanOption>(0x0, "screenshot",     "Take a single screenshot, save it to file and quit");
-
-		BooleanOption  & argDebug           = parser.add<BooleanOption>(0x0, "debug",          "Enable debug logging");
-		BooleanOption  & argHelp            = parser.add<BooleanOption>('h', "help",           "Show this help message and exit");
-
-		// parse all arguments
-		parser.process(app);
-
-		// check if debug logging is required
-		if (parser.isSet(argDebug))
-		{
-			Logger::setLogLevel(Logger::DEBUG);
-		}
-
-		// check if we need to display the usage. exit if we do.
-		if (parser.isSet(argHelp))
-		{
-			parser.showHelp(0);
-		}
-
-		QtWrapper qtWrapper(
-			argFps.getInt(parser),
-			argDisplay.getInt(parser),
-			argSizeDecimation.getInt(parser),
-			argCropLeft.getInt(parser),
-			argCropRight.getInt(parser),
-			argCropTop.getInt(parser),
-			argCropBottom.getInt(parser)
-			);
-
-		if (!qtWrapper.displayInit())
-		{
-			Error(log, "Failed to initialise the screen/display for this grabber");
-			return -1;
-		}
-
-		// set 3D mode if applicable
-		if (parser.isSet(arg3DSBS))
-		{
-			qtWrapper.setVideoMode(VideoMode::VIDEO_3DSBS);
-		}
-		else if (parser.isSet(arg3DTAB))
-		{
-			qtWrapper.setVideoMode(VideoMode::VIDEO_3DTAB);
-		}
-
-		if (parser.isSet(argScreenshot))
-		{
-			// Capture a single screenshot and finish
-			const Image<ColorRgb> &screenshot = qtWrapper.getScreenshot();
-			saveScreenshot("screenshot.png", screenshot);
-		}
-		else
-		{
-			QString host;
-			QString serviceName {QHostInfo::localHostName()};
-			int port {FLATBUFFER_DEFAULT_PORT};
-
-			// Split hostname and port (or use default port)
-			QString givenAddress = argAddress.value(parser);
-			if (!NetUtils::resolveHostPort(givenAddress, host, port))
-			{
-				throw std::runtime_error(QString("Wrong address: unable to parse address (%1)").arg(givenAddress).toStdString());
-			}
-
-			// Search available Hyperion services via mDNS, if default/localhost IP is given
-			if (host == "127.0.0.1" || host == "::1")
-			{
-#ifndef ENABLE_MDNS
-				SSDPDiscover discover;
-				host = discover.getFirstService(searchType::STY_FLATBUFSERVER);
-#endif
-				
-				QHostAddress address;
-				if (!NetUtils::resolveHostToAddress(log, host, address, port))
-				{
-					throw std::runtime_error(QString("Address could not be resolved for hostname: %2").arg(QSTRING_CSTR(host)).toStdString());
-				}
-				host = address.toString();
-			}
-
-			Info(log, "Connecting to Hyperion host: %s, port: %u using service: %s", QSTRING_CSTR(host), port, QSTRING_CSTR(serviceName));
-
-			// Create the Flabuf-connection
-			FlatBufferConnection flatbuf("Qt Standalone", host, argPriority.getInt(parser), parser.isSet(argSkipReply), port);
-
-			// Connect the screen capturing to flatbuf connection processing
-			QObject::connect(&qtWrapper, &QtWrapper::sig_screenshot, &flatbuf, &FlatBufferConnection::setImage);
-
-			// Start the capturing
-			qtWrapper.start();
-
-			// Start the application
-			QGuiApplication::exec();
-		}
+		Logger::setLogLevel(Logger::DEBUG);
 	}
-	catch (const std::runtime_error & e)
+
+	// check if we need to display the usage. exit if we do.
+	if (parser.isSet(argHelp))
 	{
-		// An error occurred. Display error and quit
-		Error(log, "%s", e.what());
-		return -1;
+		parser.showHelp(0);
+	}
+
+	QtWrapper qtWrapper(
+				argFps.getInt(parser),
+				argDisplay.getInt(parser),
+				argSizeDecimation.getInt(parser),
+				argCropLeft.getInt(parser),
+				argCropRight.getInt(parser),
+				argCropTop.getInt(parser),
+				argCropBottom.getInt(parser)
+				);
+
+	if (!qtWrapper.displayInit())
+	{
+		errorManager.errorOccurred("Failed to initialise the screen/display for this grabber");
+	}
+
+	// set 3D mode if applicable
+	if (parser.isSet(arg3DSBS))
+	{
+		qtWrapper.setVideoMode(VideoMode::VIDEO_3DSBS);
+	}
+	else if (parser.isSet(arg3DTAB))
+	{
+		qtWrapper.setVideoMode(VideoMode::VIDEO_3DTAB);
+	}
+
+	if (parser.isSet(argScreenshot))
+	{
+		// Capture a single screenshot and finish
+		const Image<ColorRgb> &screenshot = qtWrapper.getScreenshot();
+		QString fileName = "screenshot.png";
+		saveScreenshot(fileName, screenshot);
+		Info(log, "Screenshot saved as: \"%s\"", QSTRING_CSTR(fileName));
+	}
+	else
+	{
+		QString host;
+		QString serviceName {QHostInfo::localHostName()};
+		int port {FLATBUFFER_DEFAULT_PORT};
+
+		// Split hostname and port (or use default port)
+		QString givenAddress = argAddress.value(parser);
+		if (!NetUtils::resolveHostPort(givenAddress, host, port))
+		{
+			errorManager.errorOccurred(QString("Wrong address: unable to parse address (%1)").arg(givenAddress));
+		}
+
+		// Search available Hyperion services via mDNS, if default/localhost IP is given
+		if (host == "127.0.0.1" || host == "::1")
+		{
+#ifndef ENABLE_MDNS
+			SSDPDiscover discover;
+			host = discover.getFirstService(searchType::STY_FLATBUFSERVER);
+#endif
+
+			QHostAddress address;
+			if (!NetUtils::resolveHostToAddress(log, host, address, port))
+			{
+				errorManager.errorOccurred(QString("Address could not be resolved for hostname: %2").arg(QSTRING_CSTR(host)));
+			}
+			host = address.toString();
+		}
+
+		Info(log, "Connecting to Hyperion host: %s, port: %u using service: %s", QSTRING_CSTR(host), port, QSTRING_CSTR(serviceName));
+
+		// Create the Flabuf-connection
+		FlatBufferConnection flatbuf("Qt Standalone", host, argPriority.getInt(parser), parser.isSet(argSkipReply), port);
+
+		// Connect the screen capturing to flatbuf connection processing
+		QObject::connect(&qtWrapper, &QtWrapper::sig_screenshot,
+						 &flatbuf,
+						 static_cast<void (FlatBufferConnection::*)(const Image<ColorRgb>&)>(&FlatBufferConnection::setImage));
+
+
+		QObject::connect(&flatbuf, &FlatBufferConnection::isReadyToSend, [&]() {
+			Debug(log,"Start grabber");
+			qtWrapper.start();
+		});
+
+		QObject::connect(&flatbuf, &FlatBufferConnection::isDisconnected, [&]() {
+			Debug(log,"Stop grabber");
+			qtWrapper.stop();
+		});
+
+		QObject::connect(&flatbuf, &FlatBufferConnection::errorOccured, [&](QString error) {
+			Debug(log,"Stop grabber");
+			qtWrapper.stop();
+			errorManager.errorOccurred(error);
+		});
+
+		// Start the application
+		QGuiApplication::exec();
 	}
 
 	Logger::deleteInstance();
