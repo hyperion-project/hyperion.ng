cef753dfb4726ccb03024ee3a64bf18d6ed4fe72
LordGrey <48840279+Lord-Grey@users.noreply.github.com>
2025-01-25 19:52:39 +0100

    Refactor FlatBufferConnection

--- include/flatbufserver/FlatBufferConnection.h
+++ include/flatbufserver/FlatBufferConnection.h
@@ -50,36 +50,29 @@ public:
 	void setSkipReply(bool skip);
 
 	///
-	/// @brief Register a new priority with given origin
-	/// @param origin  The user friendly origin string
-	/// @param priority The priority to register
-	///
-	void setRegister(const QString& origin, int priority);
-
-	///
 	/// @brief Set all leds to the specified color
 	/// @param color The color
 	/// @param priority The priority
 	/// @param duration The duration in milliseconds
 	///
-	void setColor(const ColorRgb & color, int priority, int duration = 1);
+	void setColor(const ColorRgb & color, int duration = 1);
 
 	///
 	/// @brief Clear the given priority channel
 	/// @param priority The priority
 	///
-	void clear(int priority);
+	void clearPriority(int priority);
 
 	///
 	/// @brief Clear all priority channels
 	///
-	void clearAll();
+	void clearAllPriorities();
 
 	///
-	/// @brief Send a command message and receive its reply
-	/// @param message The message to send
+	/// @brief Check, if client is already registered
+	/// @return True, if reistered
 	///
-	void sendMessage(const uint8_t* buffer, uint32_t size);
+	bool isClientRegistered();
 
 public slots:
 	///
@@ -87,17 +80,28 @@ public slots:
 	/// @param image The image
 	///
 	void setImage(const Image<ColorRgb> &image);
+	void setImage(const QByteArray& imageData, int width, int height, int duration = -1);
+
+signals:
+
+	void isReadyToSend();
+	void isDisconnected();
+	void errorOccured(const QString& error);
 
 private slots:
 	///
 	/// @brief Try to connect to the Hyperion host
 	///
-	void connectToHost();
+	void connectToRemoteHost();
 
 	///
 	/// @brief Slot called when new data has arrived
 	///
 	void readData();
+
+	void onConnected();
+	void onDisconnected();
+
 
 signals:
 
@@ -107,6 +111,15 @@ signals:
 	void setVideoMode(VideoMode videoMode);
 
 private:
+
+	///
+	/// @brief Register a new priority with given origin
+	/// @param origin  The user friendly origin string
+	/// @param priority The priority to register
+	///
+	void registerClient(const QString& origin, int priority);
+
+	void sendMessage(const uint8_t* data, size_t size);
 
 	///
 	/// @brief Parse a reply message
@@ -132,10 +145,8 @@ private:
 	QByteArray _receiveBuffer;
 
 	QTimer _timer;
-	QAbstractSocket::SocketState  _prevSocketState;
-
 	Logger * _log;
-	flatbuffers::FlatBufferBuilder _builder;
 
-	bool _registered;
+	flatbuffers::FlatBufferBuilder _builder;
+	bool _isRegistered;
 };
