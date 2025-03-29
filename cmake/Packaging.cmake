a16e033402eddf31a97b61ce758a1d78b0ccc04d
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2025-03-29 18:54:52 +0100

    Build Hyperion on GitHub with pre-built dependencies (#1851)
    * Preparation for compilation with pre-build dependencies
    
    * c&p typo
    
    * cleanup
    
    * typo
    
    * test run
    
    * superbuild libusb on windows
    
    * macos changes
    
    * typo
    
    * set libftdi branch to master
    
    * libftdi tests
    
    * edits
    
    * set libusb/libftdi prefix
    fix libftdi patch
    
    * set CMAKE_STATIC_LIBRARY_PREFIX to lib
    
    * update
    
    * libftdi changes
    
    * test
    
    * test 2
    
    * test3
    
    * test 4
    
    * remove libftdi folder
    
    * test 5
    
    * test 6
    
    * test 7
    
    * reset
    
    * Replace static array with dynamic size allocation
    
    * Go for default inline
    
    * Correct inline update
    
    * Replace FTDI_CHECK_RESULT macro by function
    
    * Enable DEFAULT_DEV_USB_HID on all platforms by default
    
    * parse prebuiltdeps json with jq
    
    * typo
    
    * pre built run test (linux)
    
    * typo
    
    * test run
    
    * typo
    
    * verbose output
    
    * verbose echo
    
    * typo
    
    * redirect
    
    * verbose
    
    * test
    
    * map /tmp/deps on docker
    
    * chmod pre-built binaries
    
    * link libudev under linux
    
    * link libudev on linux
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * test
    
    * debug
    
    * pre-built test run
    
    * update
    
    * exclude windows
    
    * append macos pre-built cmake args
    
    * add windows
    
    * update windows path
    
    * Extraction of dxsdk and turbojpeg to speed up the step
    
    * use cmd shell
    
    * correct installer paths
    
    * use env:GITHUB_ENV on windows
    
    * another test
    
    * test
    
    * remove ninja parallel parameter
    
    * fix directx9 build
    
    * test run
    
    * test
    
    * change to hyperion-project
    
    * typo
    
    * typo 2
    
    * use pre-built deps in the codeql cpp analysis
    
    ---------
    
    Co-authored-by: Lord-Grey <lordgrey.emmel@gmail.com>

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -1,14 +1,11 @@
 # cmake file for generating distribution packages
 
 # Default packages to build
-if(APPLE)
+if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
 	set(CPACK_GENERATOR "DragNDrop")
 	set(CPACK_DMG_FORMAT "UDBZ" )
-	# Overwrite CMAKE_SYSTEM_NAME for mac (visual)
-	if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-		set(CMAKE_SYSTEM_NAME "macOS")
-	endif()
-elseif(UNIX)
+	set(CMAKE_SYSTEM_NAME "macOS")
+elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
 	set(CPACK_GENERATOR "TGZ")
 elseif(WIN32)
 	set(CPACK_GENERATOR "ZIP" "NSIS")
@@ -90,7 +87,7 @@ set(CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/cmake/linux/package
 
 # DragNDrop (macOS only)
 # https://cmake.org/cmake/help/latest/cpack_gen/dmg.html
-if(APPLE)
+if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
 	set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/cmake/macos/PackageIcon.icns" )
 	set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_BINARY_DIR}/LICENSE" )
 	set(CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/cmake/macos/Background.png" )
