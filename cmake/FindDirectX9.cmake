d1433fd58a7219ea0b5b59a02b51d3c72dab1aef
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2025-03-29 10:58:29 +0100

    fix directx9 build

--- cmake/FindDirectX9.cmake
+++ cmake/FindDirectX9.cmake
@@ -5,10 +5,8 @@
 #  DIRECTX9_FOUND, If false, do not try to use DirectX.
 
 set(DIRECTX9_PATHS
-    "$ENV{DXSDK_DIR}"
-    "$ENV{DIRECTX_ROOT}"
-    "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)"
-    "C:/Program Files/Microsoft DirectX SDK (June 2010)"
+	"C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)"
+	"C:/Program Files/Microsoft DirectX SDK (June 2010)"
 )
 
 find_path(DIRECTX9_INCLUDE_DIRS
@@ -16,8 +14,11 @@ find_path(DIRECTX9_INCLUDE_DIRS
 		d3dx9.h
 	PATHS
 		${DIRECTX9_PATHS}
+		"$ENV{DXSDK_DIR}"
+		"$ENV{DIRECTX_ROOT}"
 	PATH_SUFFIXES
-        Include
+		include
+		include/directxsdk
 )
 
 foreach(DXLIB "d3d9" "d3dx9" "DxErr")
@@ -26,31 +27,31 @@ foreach(DXLIB "d3d9" "d3dx9" "DxErr")
 			${DXLIB}
 		PATHS
 			${DIRECTX9_PATHS}
+			"$ENV{DXSDK_DIR}"
+			"$ENV{DIRECTX_ROOT}"
 		PATH_SUFFIXES
-            Lib
 			Lib/x64
+			Lib
 	)
 endforeach()
 
-set(DIRECTX9_LIBRARIES
-    ${DIRECTX9_d3d9_LIBRARY}
-    ${DIRECTX9_d3dx9_LIBRARY}
-    ${DIRECTX9_DxErr_LIBRARY}
-)
+set(DIRECTX9_LIBRARIES ${DIRECTX9_d3d9_LIBRARY} ${DIRECTX9_d3dx9_LIBRARY} ${DIRECTX9_DxErr_LIBRARY})
 
+# handle the REQUIRED arguments and set DIRECTX9_FOUND to TRUE if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(DirectX9
 	FOUND_VAR
-        DIRECTX9_FOUND
+		DIRECTX9_FOUND
 	REQUIRED_VARS
-        DIRECTX9_LIBRARIES
-		DIRECTX9_INCLUDE_DIRS
+		DIRECTX9_LIBRARIES DIRECTX9_INCLUDE_DIRS
 )
 
+mark_as_advanced(DIRECTX9_INCLUDE_DIRS DIRECTX9_d3d9_LIBRARY DIRECTX9_d3dx9_LIBRARY DIRECTX9_DxErr_LIBRARY)
+
 if(DIRECTX9_FOUND AND NOT TARGET directx9)
-	add_library(directx9 UNKNOWN IMPORTED GLOBAL)
+	add_library(directx9 INTERFACE IMPORTED GLOBAL)
 	set_target_properties(directx9 PROPERTIES
-		INTERFACE_INCLUDE_DIRECTORIES ${DIRECTX9_INCLUDE_DIRS}
-		IMPORTED_LOCATION ${DIRECTX9_LIBRARIES}
+		INTERFACE_INCLUDE_DIRECTORIES "${DIRECTX9_INCLUDE_DIRS}"
+		INTERFACE_LINK_LIBRARIES "${DIRECTX9_LIBRARIES}"
 	)
 endif()
